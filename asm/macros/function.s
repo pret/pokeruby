5b648ca2591f34c06ea51e3f3b6f9c5835533310
YamaArashi <shadow962@live.com>
2016-04-03 03:46:32 -0700

    task.c

--- asm/macros/function.s
+++ asm/macros/function.s
@@ -2,7 +2,7 @@
 	.align 2, 0
 	.global \name
 	.arm
-	.type \name, %function
+	.type \name, function
 	.endm
 
 	.macro arm_func_end name
@@ -14,14 +14,14 @@
 	.global \name
 	.thumb
 	.thumb_func
-	.type \name, %function
+	.type \name, function
 	.endm
 
 	.macro non_word_aligned_thumb_func_start name
 	.global \name
 	.thumb
 	.thumb_func
-	.type \name, %function
+	.type \name, function
 	.endm
 
 	.macro thumb_func_end name
