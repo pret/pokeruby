50cc6d4a76adf0bc361f6fd4fc570a990ae20c50
yenatch <yenatch@gmail.com>
2016-06-19 00:01:06 -0400

    Get rid of GEN_FONTS.

--- generated.mk
+++ generated.mk
@@ -12,8 +12,6 @@ include/fonts/default_palette.h
 
 GEN_LINK_HEADERS := include/link/digit_tiles.h include/link/digit_palette.h
 
-GEN_FONTS := $(GEN_FONT_HEADERS) $(GEN_LINK_HEADERS)
-
 include/fonts/font0_lat_glyphs.h: graphics/fonts/font0_lat.1bpp
 	$(BIN2C) $< sFont0LatinGlyphs >$@ -static -col 8 -pad 4
 
