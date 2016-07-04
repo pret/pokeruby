c0dde9d5c023064c6d19199cb4af67a7e197c351
YamaArashi <shadow962@live.com>
2016-07-04 12:36:20 -0700

    formatting

--- data/graphics/field_objects/field_effect_object_graphics.s
+++ data/graphics/field_objects/field_effect_object_graphics.s
@@ -1,20 +1,16 @@
 	.align 2
-
 gFieldEffectObjectPic_ShadowSmall: @ 8368528
 	.incbin "graphics/field_effect_objects/pics/shadow_small.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_ShadowMedium: @ 8368548
 	.incbin "graphics/field_effect_objects/pics/shadow_medium.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_ShadowLarge: @ 8368588
 	.incbin "graphics/field_effect_objects/pics/shadow_large.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_ShadowExtraLarge: @ 8368608
 	.incbin "graphics/field_effect_objects/pics/shadow_extra_large.4bpp"
 
@@ -22,150 +18,121 @@ gFieldEffectObjectPic_ShadowExtraLarge: @ 8368608
 	.space 0x120
 
 	.align 2
-
 gFieldEffectObjectPic_CutGrass: @ 8368B28
 	.incbin "graphics/field_effect_objects/pics/cut_grass.4bpp"
 	.incbin "graphics/field_effect_objects/pics/cut_grass.4bpp" @ included twice
 
 	.align 2
-
 gFieldEffectObjectPalette6: @ 8368B68
 	.incbin "graphics/field_effect_objects/palettes/06.gbapal"
 
 	.align 2
-
 gFieldEffectObjectPic_Ripple_0: @ 8368B88
 	.incbin "graphics/field_effect_objects/pics/ripple/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Ripple_1: @ 8368C08
 	.incbin "graphics/field_effect_objects/pics/ripple/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Ripple_2: @ 8368C88
 	.incbin "graphics/field_effect_objects/pics/ripple/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Ripple_3: @ 8368D08
 	.incbin "graphics/field_effect_objects/pics/ripple/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Ripple_4: @ 8368D88
 	.incbin "graphics/field_effect_objects/pics/ripple/4.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Ash_0: @ 8368E08
 	.incbin "graphics/field_effect_objects/pics/ash/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Ash_1: @ 8368E88
 	.incbin "graphics/field_effect_objects/pics/ash/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Ash_2: @ 8368F08
 	.incbin "graphics/field_effect_objects/pics/ash/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Ash_3: @ 8368F88
 	.incbin "graphics/field_effect_objects/pics/ash/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Ash_4: @ 8369008
 	.incbin "graphics/field_effect_objects/pics/ash/4.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Arrow_0: @ 8369088
 	.incbin "graphics/field_effect_objects/pics/arrow/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Arrow_1: @ 8369108
 	.incbin "graphics/field_effect_objects/pics/arrow/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Arrow_2: @ 8369188
 	.incbin "graphics/field_effect_objects/pics/arrow/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Arrow_3: @ 8369208
 	.incbin "graphics/field_effect_objects/pics/arrow/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Arrow_4: @ 8369288
 	.incbin "graphics/field_effect_objects/pics/arrow/4.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Arrow_5: @ 8369308
 	.incbin "graphics/field_effect_objects/pics/arrow/5.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Arrow_6: @ 8369388
 	.incbin "graphics/field_effect_objects/pics/arrow/6.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Arrow_7: @ 8369408
 	.incbin "graphics/field_effect_objects/pics/arrow/7.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPalette0: @ 8369488
 	.incbin "graphics/field_effect_objects/palettes/00.gbapal"
 
 	.align 2
-
 gFieldEffectObjectPalette1: @ 83694A8
 	.incbin "graphics/field_effect_objects/palettes/01.gbapal"
 
 	.align 2
-
 gFieldEffectObjectPic_Dust_0: @ 83694C8
 	.incbin "graphics/field_effect_objects/pics/dust/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Dust_1: @ 8369508
 	.incbin "graphics/field_effect_objects/pics/dust/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Dust_2: @ 8369548
 	.incbin "graphics/field_effect_objects/pics/dust/2.4bpp"
 
 	.incbin "graphics/field_effect_objects/unknown.bin"
 
 	.align 2
-
 gFieldEffectObjectPic_JumpTallGrass_0: @ 8369648
 	.incbin "graphics/field_effect_objects/pics/jump_tall_grass/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_JumpTallGrass_1: @ 8369688
 	.incbin "graphics/field_effect_objects/pics/jump_tall_grass/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_JumpTallGrass_2: @ 83696C8
 	.incbin "graphics/field_effect_objects/pics/jump_tall_grass/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_JumpTallGrass_3: @ 8369708
 	.incbin "graphics/field_effect_objects/pics/jump_tall_grass/3.4bpp"
 
@@ -175,518 +142,415 @@ gFieldEffectObjectPic_JumpTallGrass_3: @ 8369708
 	.incbin "graphics/field_effect_objects/pics/unused_grass/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_JumpLongGrass_0: @ 8369948
 	.incbin "graphics/field_effect_objects/pics/jump_long_grass/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_JumpLongGrass_1: @ 83699C8
 	.incbin "graphics/field_effect_objects/pics/jump_long_grass/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_JumpLongGrass_2: @ 8369A48
 	.incbin "graphics/field_effect_objects/pics/jump_long_grass/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_JumpLongGrass_3: @ 8369AC8
 	.incbin "graphics/field_effect_objects/pics/jump_long_grass/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_JumpLongGrass_4: @ 8369B48
 	.incbin "graphics/field_effect_objects/pics/jump_long_grass/4.4bpp"
 
 	.incbin "graphics/field_effect_objects/pics/jump_long_grass/unused.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_JumpLongGrass_5: @ 8369C48
 	.incbin "graphics/field_effect_objects/pics/jump_long_grass/5.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown17_0: @ 8369CC8
 	.incbin "graphics/field_effect_objects/pics/unknown_17/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown17_1: @ 8369D48
 	.incbin "graphics/field_effect_objects/pics/unknown_17/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown17_2: @ 8369DC8
 	.incbin "graphics/field_effect_objects/pics/unknown_17/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown17_3: @ 8369E48
 	.incbin "graphics/field_effect_objects/pics/unknown_17/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown17_4: @ 8369EC8
 	.incbin "graphics/field_effect_objects/pics/unknown_17/4.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown17_5: @ 8369F48
 	.incbin "graphics/field_effect_objects/pics/unknown_17/5.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown17_6: @ 8369FC8
 	.incbin "graphics/field_effect_objects/pics/unknown_17/6.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown17_7: @ 836A048
 	.incbin "graphics/field_effect_objects/pics/unknown_17/7.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown18_0: @ 836A0C8
 	.incbin "graphics/field_effect_objects/pics/unknown_18/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown18_1: @ 836A148
 	.incbin "graphics/field_effect_objects/pics/unknown_18/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown18_2: @ 836A1C8
 	.incbin "graphics/field_effect_objects/pics/unknown_18/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown18_3: @ 836A248
 	.incbin "graphics/field_effect_objects/pics/unknown_18/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_LongGrass_0: @ 836A2C8
 	.incbin "graphics/field_effect_objects/pics/long_grass/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_LongGrass_1: @ 836A348
 	.incbin "graphics/field_effect_objects/pics/long_grass/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_LongGrass_2: @ 836A3C8
 	.incbin "graphics/field_effect_objects/pics/long_grass/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_LongGrass_3: @ 836A448
 	.incbin "graphics/field_effect_objects/pics/long_grass/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_TallGrass_0: @ 836A4C8
 	.incbin "graphics/field_effect_objects/pics/tall_grass/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_TallGrass_1: @ 836A548
 	.incbin "graphics/field_effect_objects/pics/tall_grass/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_TallGrass_2: @ 836A5C8
 	.incbin "graphics/field_effect_objects/pics/tall_grass/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_TallGrass_3: @ 836A648
 	.incbin "graphics/field_effect_objects/pics/tall_grass/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_TallGrass_4: @ 836A6C8
 	.incbin "graphics/field_effect_objects/pics/tall_grass/4.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_ShortGrass_0: @ 836A748
 	.incbin "graphics/field_effect_objects/pics/short_grass/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_ShortGrass_1: @ 836A7C8
 	.incbin "graphics/field_effect_objects/pics/short_grass/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_SandFootprints_0: @ 836A848
 	.incbin "graphics/field_effect_objects/pics/sand_footprints/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_SandFootprints_1: @ 836A8C8
 	.incbin "graphics/field_effect_objects/pics/sand_footprints/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_DeepSandFootprints_0: @ 836A948
 	.incbin "graphics/field_effect_objects/pics/deep_sand_footprints/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_DeepSandFootprints_1: @ 836A9C8
 	.incbin "graphics/field_effect_objects/pics/deep_sand_footprints/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_BikeTireTracks_0: @ 836AA48
 	.incbin "graphics/field_effect_objects/pics/bike_tire_tracks/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_BikeTireTracks_1: @ 836AAC8
 	.incbin "graphics/field_effect_objects/pics/bike_tire_tracks/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_BikeTireTracks_2: @ 836AB48
 	.incbin "graphics/field_effect_objects/pics/bike_tire_tracks/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_BikeTireTracks_3: @ 836ABC8
 	.incbin "graphics/field_effect_objects/pics/bike_tire_tracks/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown19_0: @ 836AC48
 	.incbin "graphics/field_effect_objects/pics/unknown_19/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown19_1: @ 836ACC8
 	.incbin "graphics/field_effect_objects/pics/unknown_19/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown19_2: @ 836AD48
 	.incbin "graphics/field_effect_objects/pics/unknown_19/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown19_3: @ 836ADC8
 	.incbin "graphics/field_effect_objects/pics/unknown_19/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown29_0: @ 836AE48
 	.incbin "graphics/field_effect_objects/pics/unknown_29/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown29_1: @ 836AE88
 	.incbin "graphics/field_effect_objects/pics/unknown_29/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown29_2: @ 836AEC8
 	.incbin "graphics/field_effect_objects/pics/unknown_29/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_JumpBigSplash_0: @ 836AF08
 	.incbin "graphics/field_effect_objects/pics/jump_big_splash/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_JumpBigSplash_1: @ 836AF88
 	.incbin "graphics/field_effect_objects/pics/jump_big_splash/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_JumpBigSplash_2: @ 836B008
 	.incbin "graphics/field_effect_objects/pics/jump_big_splash/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_JumpBigSplash_3: @ 836B088
 	.incbin "graphics/field_effect_objects/pics/jump_big_splash/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Splash_0: @ 836B108
 	.incbin "graphics/field_effect_objects/pics/splash/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Splash_1: @ 836B148
 	.incbin "graphics/field_effect_objects/pics/splash/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_JumpSmallSplash_0: @ 836B188
 	.incbin "graphics/field_effect_objects/pics/jump_small_splash/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_JumpSmallSplash_1: @ 836B1C8
 	.incbin "graphics/field_effect_objects/pics/jump_small_splash/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_JumpSmallSplash_2: @ 836B208
 	.incbin "graphics/field_effect_objects/pics/jump_small_splash/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown20_0: @ 836B248
 	.incbin "graphics/field_effect_objects/pics/unknown_20/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown20_1: @ 836B2C8
 	.incbin "graphics/field_effect_objects/pics/unknown_20/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown20_2: @ 836B348
 	.incbin "graphics/field_effect_objects/pics/unknown_20/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown20_3: @ 836B3C8
 	.incbin "graphics/field_effect_objects/pics/unknown_20/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown20_4: @ 836B448
 	.incbin "graphics/field_effect_objects/pics/unknown_20/4.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_TreeDisguise_0: @ 836B4C8
 	.incbin "graphics/field_effect_objects/pics/tree_disguise/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_TreeDisguise_1: @ 836B5C8
 	.incbin "graphics/field_effect_objects/pics/tree_disguise/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_TreeDisguise_2: @ 836B6C8
 	.incbin "graphics/field_effect_objects/pics/tree_disguise/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_TreeDisguise_3: @ 836B7C8
 	.incbin "graphics/field_effect_objects/pics/tree_disguise/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_TreeDisguise_4: @ 836B8C8
 	.incbin "graphics/field_effect_objects/pics/tree_disguise/4.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_TreeDisguise_5: @ 836B9C8
 	.incbin "graphics/field_effect_objects/pics/tree_disguise/5.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_TreeDisguise_6: @ 836BAC8
 	.incbin "graphics/field_effect_objects/pics/tree_disguise/6.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_MountainDisguise_0: @ 836BBC8
 	.incbin "graphics/field_effect_objects/pics/mountain_disguise/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_MountainDisguise_1: @ 836BCC8
 	.incbin "graphics/field_effect_objects/pics/mountain_disguise/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_MountainDisguise_2: @ 836BDC8
 	.incbin "graphics/field_effect_objects/pics/mountain_disguise/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_MountainDisguise_3: @ 836BEC8
 	.incbin "graphics/field_effect_objects/pics/mountain_disguise/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_MountainDisguise_4: @ 836BFC8
 	.incbin "graphics/field_effect_objects/pics/mountain_disguise/4.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_MountainDisguise_5: @ 836C0C8
 	.incbin "graphics/field_effect_objects/pics/mountain_disguise/5.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_MountainDisguise_6: @ 836C1C8
 	.incbin "graphics/field_effect_objects/pics/mountain_disguise/6.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_SandDisguise_0: @ 836C2C8
 	.incbin "graphics/field_effect_objects/pics/sand_disguise/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_SandDisguise_1: @ 836C3C8
 	.incbin "graphics/field_effect_objects/pics/sand_disguise/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_SandDisguise_2: @ 836C4C8
 	.incbin "graphics/field_effect_objects/pics/sand_disguise/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_SandDisguise_3: @ 836C5C8
 	.incbin "graphics/field_effect_objects/pics/sand_disguise/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_SandDisguise_4: @ 836C6C8
 	.incbin "graphics/field_effect_objects/pics/sand_disguise/4.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_SandDisguise_5: @ 836C7C8
 	.incbin "graphics/field_effect_objects/pics/sand_disguise/5.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_SandDisguise_6: @ 836C8C8
 	.incbin "graphics/field_effect_objects/pics/sand_disguise/6.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_HotSpringsWater: @ 836C9C8
 	.incbin "graphics/field_effect_objects/pics/hot_springs_water.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPalette2: @ 836CA48
 	.incbin "graphics/field_effect_objects/palettes/02.gbapal"
 
 	.align 2
-
 gFieldEffectObjectPic_PopOutOfAsh_0: @ 836CA68
 	.incbin "graphics/field_effect_objects/pics/pop_out_of_ash/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_PopOutOfAsh_1: @ 836CAE8
 	.incbin "graphics/field_effect_objects/pics/pop_out_of_ash/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_PopOutOfAsh_2: @ 836CB68
 	.incbin "graphics/field_effect_objects/pics/pop_out_of_ash/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_PopOutOfAsh_3: @ 836CBE8
 	.incbin "graphics/field_effect_objects/pics/pop_out_of_ash/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_PopOutOfAsh_4: @ 836CC68
 	.incbin "graphics/field_effect_objects/pics/pop_out_of_ash/4.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown33_0: @ 836CCE8
 	.incbin "graphics/field_effect_objects/pics/unknown_33/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown33_1: @ 836CD68
 	.incbin "graphics/field_effect_objects/pics/unknown_33/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown33_2: @ 836CDE8
 	.incbin "graphics/field_effect_objects/pics/unknown_33/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown33_3: @ 836CE68
 	.incbin "graphics/field_effect_objects/pics/unknown_33/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown33_4: @ 836CEE8
 	.incbin "graphics/field_effect_objects/pics/unknown_33/4.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Bubbles_0: @ 836CF68
 	.incbin "graphics/field_effect_objects/pics/bubbles/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Bubbles_1: @ 836D068
 	.incbin "graphics/field_effect_objects/pics/bubbles/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Bubbles_2: @ 836D168
 	.incbin "graphics/field_effect_objects/pics/bubbles/2.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Bubbles_3: @ 836D268
 	.incbin "graphics/field_effect_objects/pics/bubbles/3.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Bubbles_4: @ 836D368
 	.incbin "graphics/field_effect_objects/pics/bubbles/4.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Bubbles_5: @ 836D468
 	.incbin "graphics/field_effect_objects/pics/bubbles/5.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Bubbles_6: @ 836D568
 	.incbin "graphics/field_effect_objects/pics/bubbles/6.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Bubbles_7: @ 836D668
 	.incbin "graphics/field_effect_objects/pics/bubbles/7.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown35_0: @ 836D768
 	.incbin "graphics/field_effect_objects/pics/unknown_35/0.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPic_Unknown35_1: @ 836D7E8
 	.incbin "graphics/field_effect_objects/pics/unknown_35/1.4bpp"
 
 	.align 2
-
 gFieldEffectObjectPalette3: @ 836D868
 	.incbin "graphics/field_effect_objects/palettes/03.gbapal"
 
 	.align 2
-
 gFieldEffectObjectPic_Bird: @ 836D888
 	.incbin "graphics/field_effect_objects/pics/bird.4bpp"
