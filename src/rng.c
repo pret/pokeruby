4b9306f03a4d16f47621feb3cb273d5c086e6e5d
camthesaxman <cameronghall@cox.net>
2016-07-23 17:49:14 -0500

    decompile rng functions

---
+++ src/rng.c
@@ -0,0 +1,14 @@
+#include "global.h"
+
+extern u32 gRngValue;
+
+u16 Random(void)
+{
+	gRngValue = 0x41c64e6d * gRngValue + 0x00006073;
+	return gRngValue >> 16;
+}
+
+void SeedRng(u16 seed)
+{
+	gRngValue = seed;
+}
