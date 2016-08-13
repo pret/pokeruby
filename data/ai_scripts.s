1837766d9ea255d413c9fb0141d60d0a4d5799d9
ProjectRevoTPP <projectrevotpp@hotmail.com>
2016-08-12 23:28:53 -0400

    remerge local work

--- data/ai_scripts.s
+++ data/ai_scripts.s
@@ -3,8 +3,117 @@
 
 	.section script_data, "aw", %progbits
 
+	.align 2
 gUnknown_081DA01C:: @ 81DA01C
-	.incbin "baserom.gba", 0x001da01c, 0x20fc
+	.4byte Unknown_081DA09C
+	.4byte Unknown_081DA86D
+	.4byte Unknown_081DBE97
+	.4byte Unknown_081DBEB5
+	.4byte Unknown_081DBF17
+	.4byte Unknown_081DBF07
+	.4byte Unknown_081DBF3E
+	.4byte Unknown_081DBF63
+	.4byte Unknown_081DBF64
+	.4byte Unknown_081DC0B9
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC115
+	.4byte Unknown_081DC0D0
+	.4byte Unknown_081DC0FE
+	.4byte Unknown_081DC105
+Unknown_081DA09C::
+	.incbin "baserom.gba", 0x001da09c, 0x7D1
+Unknown_081DA86D::
+	.incbin "baserom.gba", 0x001da86d, 0x162A
+Unknown_081DBE97::
+	.incbin "baserom.gba", 0x001dbe97, 0x1E
+Unknown_081DBEB5::
+	.incbin "baserom.gba", 0x001dbeb5, 0x52
+Unknown_081DBF07::
+	.incbin "baserom.gba", 0x001dbf07, 0x10
+Unknown_081DBF17::
+	.incbin "baserom.gba", 0x001dbf17, 0x27
+Unknown_081DBF3E::
+	.incbin "baserom.gba", 0x001dbf3e, 0x25
+Unknown_081DBF63::
+	.incbin "baserom.gba", 0x001dbf63, 0x1
+Unknown_081DBF64::
+	.incbin "baserom.gba", 0x001dbf64, 0x155
+Unknown_081DC0B9::
+	.incbin "baserom.gba", 0x001dc0b9, 0x17
+Unknown_081DC0D0::
+	.incbin "baserom.gba", 0x001dc0d0, 0x2E
+Unknown_081DC0FE::
+	.incbin "baserom.gba", 0x001dc0fe, 0x7
+Unknown_081DC105::
+	.incbin "baserom.gba", 0x001dc105, 0x10
+Unknown_081DC115::
+	.incbin "baserom.gba", 0x001dc115, 0x3
 
+	.align 2
 gUnknown_081DC118:: @ 81DC118
-	.incbin "baserom.gba", 0x001dc118, 0x9dc
+	.4byte Unknown_081DC7FB
+	.4byte Unknown_081DC348
+	.4byte Unknown_081DC27F
+	.4byte Unknown_081DC2AB
+	.4byte Unknown_081DCA4C
+	.4byte Unknown_081DC3C7
+	.4byte Unknown_081DC7DE
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.4byte Unknown_081DCAF3
+	.incbin "baserom.gba", 0x001dc198, 0xE7 @ unreferenced?
+Unknown_081DC27F:
+	.incbin "baserom.gba", 0x001dc27f, 0x2C
+Unknown_081DC2AB:
+	.incbin "baserom.gba", 0x001dc2ab, 0x9D
+Unknown_081DC348:
+	.incbin "baserom.gba", 0x001dc348, 0x7F
+Unknown_081DC3C7:
+	.incbin "baserom.gba", 0x001dc3c7, 0x417
+Unknown_081DC7DE:
+	.incbin "baserom.gba", 0x001dc7de, 0x1D
+Unknown_081DC7FB:
+	.incbin "baserom.gba", 0x001dc7fb, 0x251
+Unknown_081DCA4C:
+	.incbin "baserom.gba", 0x001dca4c, 0xA7
+Unknown_081DCAF3:
+	.incbin "baserom.gba", 0x001dcaf3, 0x1
