e092636caf708a8b21d9bfe3f138de6267c32cbe
yenatch <yenatch@gmail.com>
2016-08-18 00:09:08 -0400

    Add battle ai scripts.

--- constants/battle.s
+++ constants/battle.s
@@ -11,3 +11,20 @@
 .equiv S_SUBSTITUTE,   0x1000000
 .equiv S_MEAN_LOOK,    0x4000000
 .equiv S_NIGHTMARE,    0x8000000
+
+.equiv TARGET, 0
+.equiv USER, 1
+
+.equiv WEATHER_SUN, 0
+.equiv WEATHER_RAIN, 1
+.equiv WEATHER_SANDSTORM, 2
+.equiv WEATHER_HAIL, 3
+
+	enum_start 1
+	enum ATTACK
+	enum DEFENSE
+	enum SPEED
+	enum SP_ATTACK
+	enum SP_DEFENSE
+	enum ACCURACY
+	enum EVASION
--- data/contest_ai_scripts.s
+++ data/contest_ai_scripts.s
@@ -3,5 +3,6 @@
 
 	.section script_data, "aw", %progbits
 
+	.align 2
 gUnknown_081DC118:: @ 81DC118
 	.incbin "baserom.gba", 0x001dc118, 0x9dc
