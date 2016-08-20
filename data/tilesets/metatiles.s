1fce53f2b7bf2517c2436e4f8d7d4cc5ff95314e
yenatch <yenatch@gmail.com>
2016-08-20 16:54:16 -0400

    Create files for tileset metatiles.

--- data/tilesets/metatiles.s
+++ data/tilesets/metatiles.s
@@ -1,529 +1,424 @@
 	.align 1
-
 gMetatiles_General:: @ 8259C1C
-	.incbin "baserom.gba", 0x259c1c, 0x2000
+	.incbin "data/tilesets/primary/general/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_General:: @ 825BC1C
-	.incbin "baserom.gba", 0x25bc1c, 0x400
+	.incbin "data/tilesets/primary/general/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Petalburg:: @ 825C01C
-	.incbin "baserom.gba", 0x25c01c, 0x900
+	.incbin "data/tilesets/secondary/petalburg/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Petalburg:: @ 825C91C
-	.incbin "baserom.gba", 0x25c91c, 0x120
+	.incbin "data/tilesets/secondary/petalburg/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Rustboro:: @ 825CA3C
-	.incbin "baserom.gba", 0x25ca3c, 0x15e0
+	.incbin "data/tilesets/secondary/rustboro/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Rustboro:: @ 825E01C
-	.incbin "baserom.gba", 0x25e01c, 0x2bc
+	.incbin "data/tilesets/secondary/rustboro/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Dewford:: @ 825E2D8
-	.incbin "baserom.gba", 0x25e2d8, 0x900
+	.incbin "data/tilesets/secondary/dewford/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Dewford:: @ 825EBD8
-	.incbin "baserom.gba", 0x25ebd8, 0x120
+	.incbin "data/tilesets/secondary/dewford/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Slateport:: @ 825ECF8
-	.incbin "baserom.gba", 0x25ecf8, 0x16c0
+	.incbin "data/tilesets/secondary/slateport/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Slateport:: @ 82603B8
-	.incbin "baserom.gba", 0x2603b8, 0x2d8
+	.incbin "data/tilesets/secondary/slateport/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Mauville:: @ 8260690
-	.incbin "baserom.gba", 0x260690, 0x1b20
+	.incbin "data/tilesets/secondary/mauville/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Mauville:: @ 82621B0
-	.incbin "baserom.gba", 0x2621b0, 0x364
+	.incbin "data/tilesets/secondary/mauville/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Lavaridge:: @ 8262514
-	.incbin "baserom.gba", 0x262514, 0x11e0
+	.incbin "data/tilesets/secondary/lavaridge/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Lavaridge:: @ 82636F4
-	.incbin "baserom.gba", 0x2636f4, 0x23c
+	.incbin "data/tilesets/secondary/lavaridge/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Fallarbor:: @ 8263930
-	.incbin "baserom.gba", 0x263930, 0x1520
+	.incbin "data/tilesets/secondary/fallarbor/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Fallarbor:: @ 8264E50
-	.incbin "baserom.gba", 0x264e50, 0x2a4
+	.incbin "data/tilesets/secondary/fallarbor/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Fortree:: @ 82650F4
-	.incbin "baserom.gba", 0x2650f4, 0x10b0
+	.incbin "data/tilesets/secondary/fortree/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Fortree:: @ 82661A4
-	.incbin "baserom.gba", 0x2661a4, 0x216
+	.incbin "data/tilesets/secondary/fortree/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Lilycove:: @ 82663BA
-	.incbin "baserom.gba", 0x2663ba, 0x15f0
+	.incbin "data/tilesets/secondary/lilycove/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Lilycove:: @ 82679AA
-	.incbin "baserom.gba", 0x2679aa, 0x2be
+	.incbin "data/tilesets/secondary/lilycove/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Mossdeep:: @ 8267C68
-	.incbin "baserom.gba", 0x267c68, 0x16b0
+	.incbin "data/tilesets/secondary/mossdeep/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Mossdeep:: @ 8269318
-	.incbin "baserom.gba", 0x269318, 0x2d6
+	.incbin "data/tilesets/secondary/mossdeep/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_EverGrande:: @ 82695EE
-	.incbin "baserom.gba", 0x2695ee, 0xa80
+	.incbin "data/tilesets/secondary/ever_grande/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_EverGrande:: @ 826A06E
-	.incbin "baserom.gba", 0x26a06e, 0x150
+	.incbin "data/tilesets/secondary/ever_grande/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Pacifidlog:: @ 826A1BE
-	.incbin "baserom.gba", 0x26a1be, 0xbf0
+	.incbin "data/tilesets/secondary/pacifidlog/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Pacifidlog:: @ 826ADAE
-	.incbin "baserom.gba", 0x26adae, 0x17e
+	.incbin "data/tilesets/secondary/pacifidlog/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Sootopolis:: @ 826AF2C
-	.incbin "baserom.gba", 0x26af2c, 0xfe0
+	.incbin "data/tilesets/secondary/sootopolis/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Sootopolis:: @ 826BF0C
-	.incbin "baserom.gba", 0x26bf0c, 0x1fc
+	.incbin "data/tilesets/secondary/sootopolis/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Building:: @ 826C108
-	.incbin "baserom.gba", 0x26c108, 0x80
+	.incbin "data/tilesets/primary/building/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Building:: @ 826C188
-	.incbin "baserom.gba", 0x26c188, 0x10
+	.incbin "data/tilesets/primary/building/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Shop:: @ 826C198
-	.incbin "baserom.gba", 0x26c198, 0x1180
+	.incbin "data/tilesets/secondary/shop/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Shop:: @ 826D318
-	.incbin "baserom.gba", 0x26d318, 0x230
+	.incbin "data/tilesets/secondary/shop/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_PokemonCenter:: @ 826D548
-	.incbin "baserom.gba", 0x26d548, 0xc60
+	.incbin "data/tilesets/secondary/pokemon_center/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_PokemonCenter:: @ 826E1A8
-	.incbin "baserom.gba", 0x26e1a8, 0x18c
+	.incbin "data/tilesets/secondary/pokemon_center/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Cave:: @ 826E334
-	.incbin "baserom.gba", 0x26e334, 0x19e0
+	.incbin "data/tilesets/secondary/cave/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Cave:: @ 826FD14
-	.incbin "baserom.gba", 0x26fd14, 0x33c
+	.incbin "data/tilesets/secondary/cave/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_PokemonSchool:: @ 8270050
-	.incbin "baserom.gba", 0x270050, 0x3a0
+	.incbin "data/tilesets/secondary/pokemon_school/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_PokemonSchool:: @ 82703F0
-	.incbin "baserom.gba", 0x2703f0, 0x74
+	.incbin "data/tilesets/secondary/pokemon_school/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_PokemonFanClub:: @ 8270464
-	.incbin "baserom.gba", 0x270464, 0x680
+	.incbin "data/tilesets/secondary/pokemon_fan_club/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_PokemonFanClub:: @ 8270AE4
-	.incbin "baserom.gba", 0x270ae4, 0xd0
+	.incbin "data/tilesets/secondary/pokemon_fan_club/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Unused1:: @ 8270BB4
-	.incbin "baserom.gba", 0x270bb4, 0x20
+	.incbin "data/tilesets/secondary/unused_1/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Unused1:: @ 8270BD4
-	.incbin "baserom.gba", 0x270bd4, 0x4
+	.incbin "data/tilesets/secondary/unused_1/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_MeteorFalls:: @ 8270BD8
-	.incbin "baserom.gba", 0x270bd8, 0x9f0
+	.incbin "data/tilesets/secondary/meteor_falls/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_MeteorFalls:: @ 82715C8
-	.incbin "baserom.gba", 0x2715c8, 0x13e
+	.incbin "data/tilesets/secondary/meteor_falls/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_OceanicMuseum:: @ 8271706
-	.incbin "baserom.gba", 0x271706, 0x650
+	.incbin "data/tilesets/secondary/oceanic_museum/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_OceanicMuseum:: @ 8271D56
-	.incbin "baserom.gba", 0x271d56, 0xca
+	.incbin "data/tilesets/secondary/oceanic_museum/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_CableClub:: @ 8271E20
-	.incbin "baserom.gba", 0x271e20, 0x1000
+	.incbin "data/tilesets/secondary/cable_club/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_CableClub:: @ 8272E20
-	.incbin "baserom.gba", 0x272e20, 0x200
+	.incbin "data/tilesets/secondary/cable_club/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_SeashoreHouse:: @ 8273020
-	.incbin "baserom.gba", 0x273020, 0x380
+	.incbin "data/tilesets/secondary/seashore_house/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_SeashoreHouse:: @ 82733A0
-	.incbin "baserom.gba", 0x2733a0, 0x70
+	.incbin "data/tilesets/secondary/seashore_house/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_PrettyPetalFlowerShop:: @ 8273410
-	.incbin "baserom.gba", 0x273410, 0x480
+	.incbin "data/tilesets/secondary/pretty_petal_flower_shop/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_PrettyPetalFlowerShop:: @ 8273890
-	.incbin "baserom.gba", 0x273890, 0x90
+	.incbin "data/tilesets/secondary/pretty_petal_flower_shop/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_PokemonDayCare:: @ 8273920
-	.incbin "baserom.gba", 0x273920, 0x440
+	.incbin "data/tilesets/secondary/pokemon_day_care/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_PokemonDayCare:: @ 8273D60
-	.incbin "baserom.gba", 0x273d60, 0x88
+	.incbin "data/tilesets/secondary/pokemon_day_care/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Facility:: @ 8273DE8
-	.incbin "baserom.gba", 0x273de8, 0x1fe0
+	.incbin "data/tilesets/secondary/facility/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Facility:: @ 8275DC8
-	.incbin "baserom.gba", 0x275dc8, 0x3fc
+	.incbin "data/tilesets/secondary/facility/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_BikeShop:: @ 82761C4
-	.incbin "baserom.gba", 0x2761c4, 0xf80
+	.incbin "data/tilesets/secondary/bike_shop/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_BikeShop:: @ 8277144
-	.incbin "baserom.gba", 0x277144, 0x1f0
+	.incbin "data/tilesets/secondary/bike_shop/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_RusturfTunnel:: @ 8277334
-	.incbin "baserom.gba", 0x277334, 0x530
+	.incbin "data/tilesets/secondary/rusturf_tunnel/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_RusturfTunnel:: @ 8277864
-	.incbin "baserom.gba", 0x277864, 0xa6
+	.incbin "data/tilesets/secondary/rusturf_tunnel/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_SecretBaseSecondary:: @ 827790A
-	.incbin "baserom.gba", 0x27790a, 0x1440
+	.incbin "data/tilesets/secondary/secret_base/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_SecretBaseSecondary:: @ 8278D4A
-	.incbin "baserom.gba", 0x278d4a, 0x288
+	.incbin "data/tilesets/secondary/secret_base/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_InsideOfTruck:: @ 8278FD2
-	.incbin "baserom.gba", 0x278fd2, 0x260
+	.incbin "data/tilesets/secondary/inside_of_truck/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_InsideOfTruck:: @ 8279232
-	.incbin "baserom.gba", 0x279232, 0x4c
+	.incbin "data/tilesets/secondary/inside_of_truck/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Contest:: @ 827927E
-	.incbin "baserom.gba", 0x27927e, 0xe00
+	.incbin "data/tilesets/secondary/contest/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Contest:: @ 827A07E
-	.incbin "baserom.gba", 0x27a07e, 0x1c0
+	.incbin "data/tilesets/secondary/contest/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_LilycoveMuseum:: @ 827A23E
-	.incbin "baserom.gba", 0x27a23e, 0x8f0
+	.incbin "data/tilesets/secondary/lilycove_museum/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_LilycoveMuseum:: @ 827AB2E
-	.incbin "baserom.gba", 0x27ab2e, 0x11e
+	.incbin "data/tilesets/secondary/lilycove_museum/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_BrendansMaysHouse:: @ 827AC4C
-	.incbin "baserom.gba", 0x27ac4c, 0xab0
+	.incbin "data/tilesets/secondary/brendans_mays_house/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_BrendansMaysHouse:: @ 827B6FC
-	.incbin "baserom.gba", 0x27b6fc, 0x156
+	.incbin "data/tilesets/secondary/brendans_mays_house/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Lab:: @ 827B852
-	.incbin "baserom.gba", 0x27b852, 0x9a0
+	.incbin "data/tilesets/secondary/lab/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Lab:: @ 827C1F2
-	.incbin "baserom.gba", 0x27c1f2, 0x134
+	.incbin "data/tilesets/secondary/lab/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Underwater:: @ 827C326
-	.incbin "baserom.gba", 0x27c326, 0xec0
+	.incbin "data/tilesets/secondary/underwater/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Underwater:: @ 827D1E6
-	.incbin "baserom.gba", 0x27d1e6, 0x1d8
+	.incbin "data/tilesets/secondary/underwater/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_GenericBuilding:: @ 827D3BE
-	.incbin "baserom.gba", 0x27d3be, 0x2000
+	.incbin "data/tilesets/secondary/generic_building/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_GenericBuilding:: @ 827F3BE
-	.incbin "baserom.gba", 0x27f3be, 0x400
+	.incbin "data/tilesets/secondary/generic_building/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_MauvilleGameCorner:: @ 827F7BE
-	.incbin "baserom.gba", 0x27f7be, 0x600
+	.incbin "data/tilesets/secondary/mauville_game_corner/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_MauvilleGameCorner:: @ 827FDBE
-	.incbin "baserom.gba", 0x27fdbe, 0xc0
+	.incbin "data/tilesets/secondary/mauville_game_corner/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Unused2:: @ 827FE7E
-	.incbin "baserom.gba", 0x27fe7e, 0x3a0
+	.incbin "data/tilesets/secondary/unused_2/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Unused2:: @ 828021E
-	.incbin "baserom.gba", 0x28021e, 0x74
+	.incbin "data/tilesets/secondary/unused_2/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_RustboroGym:: @ 8280292
-	.incbin "baserom.gba", 0x280292, 0x380
+	.incbin "data/tilesets/secondary/rustboro_gym/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_RustboroGym:: @ 8280612
-	.incbin "baserom.gba", 0x280612, 0x70
+	.incbin "data/tilesets/secondary/rustboro_gym/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_DewfordGym:: @ 8280682
-	.incbin "baserom.gba", 0x280682, 0x3d0
+	.incbin "data/tilesets/secondary/dewford_gym/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_DewfordGym:: @ 8280A52
-	.incbin "baserom.gba", 0x280a52, 0x7a
+	.incbin "data/tilesets/secondary/dewford_gym/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_MauvilleGym:: @ 8280ACC
-	.incbin "baserom.gba", 0x280acc, 0x750
+	.incbin "data/tilesets/secondary/mauville_gym/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_MauvilleGym:: @ 828121C
-	.incbin "baserom.gba", 0x28121c, 0xea
+	.incbin "data/tilesets/secondary/mauville_gym/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_LavaridgeGym:: @ 8281306
-	.incbin "baserom.gba", 0x281306, 0x2a0
+	.incbin "data/tilesets/secondary/lavaridge_gym/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_LavaridgeGym:: @ 82815A6
-	.incbin "baserom.gba", 0x2815a6, 0x54
+	.incbin "data/tilesets/secondary/lavaridge_gym/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_PetalburgGym:: @ 82815FA
-	.incbin "baserom.gba", 0x2815fa, 0x8c0
+	.incbin "data/tilesets/secondary/petalburg_gym/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_PetalburgGym:: @ 8281EBA
-	.incbin "baserom.gba", 0x281eba, 0x118
+	.incbin "data/tilesets/secondary/petalburg_gym/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_FortreeGym:: @ 8281FD2
-	.incbin "baserom.gba", 0x281fd2, 0x350
+	.incbin "data/tilesets/secondary/fortree_gym/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_FortreeGym:: @ 8282322
-	.incbin "baserom.gba", 0x282322, 0x6a
+	.incbin "data/tilesets/secondary/fortree_gym/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_MossdeepGym:: @ 828238C
-	.incbin "baserom.gba", 0x28238c, 0x500
+	.incbin "data/tilesets/secondary/mossdeep_gym/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_MossdeepGym:: @ 828288C
-	.incbin "baserom.gba", 0x28288c, 0xa0
+	.incbin "data/tilesets/secondary/mossdeep_gym/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_SootopolisGym:: @ 828292C
-	.incbin "baserom.gba", 0x28292c, 0x630
+	.incbin "data/tilesets/secondary/sootopolis_gym/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_SootopolisGym:: @ 8282F5C
-	.incbin "baserom.gba", 0x282f5c, 0xc6
+	.incbin "data/tilesets/secondary/sootopolis_gym/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_TrickHousePuzzle:: @ 8283022
-	.incbin "baserom.gba", 0x283022, 0x950
+	.incbin "data/tilesets/secondary/trick_house_puzzle/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_TrickHousePuzzle:: @ 8283972
-	.incbin "baserom.gba", 0x283972, 0x12a
+	.incbin "data/tilesets/secondary/trick_house_puzzle/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_Ship:: @ 8283A9C
-	.incbin "baserom.gba", 0x283a9c, 0xfc0
+	.incbin "data/tilesets/secondary/ship/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_Ship:: @ 8284A5C
-	.incbin "baserom.gba", 0x284a5c, 0x1f8
+	.incbin "data/tilesets/secondary/ship/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_SecretBasePrimary:: @ 8284C54
-	.incbin "baserom.gba", 0x284c54, 0x20
+	.incbin "data/tilesets/primary/secret_base/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_SecretBasePrimary:: @ 8284C74
-	.incbin "baserom.gba", 0x284c74, 0x4
+	.incbin "data/tilesets/primary/secret_base/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_EliteFour:: @ 8284C78
-	.incbin "baserom.gba", 0x284c78, 0x14b0
+	.incbin "data/tilesets/secondary/elite_four/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_EliteFour:: @ 8286128
-	.incbin "baserom.gba", 0x286128, 0x296
+	.incbin "data/tilesets/secondary/elite_four/metatile_attributes.bin"
 
 	.align 1
-
 gMetatiles_BattleTower:: @ 82863BE
-	.incbin "baserom.gba", 0x2863be, 0x830
+	.incbin "data/tilesets/secondary/battle_tower/metatiles.bin"
 
 	.align 1
-
 gMetatileAttributes_BattleTower:: @ 8286BEE
-	.incbin "baserom.gba", 0x286bee, 0x106
+	.incbin "data/tilesets/secondary/battle_tower/metatile_attributes.bin"
+
