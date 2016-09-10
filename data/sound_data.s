b613eb4301fc0719fba651a678f1f640c8fe9543
Marcus Huderle <huderlem@gmail.com>
2016-09-10 11:16:30 -0700

    Move data/music/ to sound/

--- data/sound_data.s
+++ data/sound_data.s
@@ -4,10 +4,8 @@
 	.include "asm/macros/music_voice.s"
 
 	.include "sound/voice_groups.s"
-	.include "data/music/keysplit_tables.s"
-	.include "data/music/programmable_wave_data.s"
-
+	.include "sound/keysplit_tables.s"
+	.include "sound/programmable_wave_data.s"
 	.include "sound/music_player_table.s"
 	.include "sound/song_table.s"
-
-	.include "data/music/direct_sound_data.s"
+	.include "sound/direct_sound_data.s"
---
+++ sound/programmable_wave_data.s
@@ -0,0 +1,29 @@
+ProgrammableWaveData_84553BC: @ 84553BC
+	.incbin "sound/programmable_wave_samples/84553BC.pcm"
+
+ProgrammableWaveData_84553CC: @ 84553CC
+	.incbin "sound/programmable_wave_samples/84553CC.pcm"
+
+ProgrammableWaveData_84553DC: @ 84553DC
+	.incbin "sound/programmable_wave_samples/84553DC.pcm"
+
+ProgrammableWaveData_84553EC: @ 84553EC
+	.incbin "sound/programmable_wave_samples/84553EC.pcm"
+
+ProgrammableWaveData_84553FC: @ 84553FC
+	.incbin "sound/programmable_wave_samples/84553FC.pcm"
+
+ProgrammableWaveData_845540C: @ 845540C
+	.incbin "sound/programmable_wave_samples/845540C.pcm"
+
+ProgrammableWaveData_845541C: @ 845541C
+	.incbin "sound/programmable_wave_samples/845541C.pcm"
+
+ProgrammableWaveData_845542C: @ 845542C
+	.incbin "sound/programmable_wave_samples/845542C.pcm"
+
+ProgrammableWaveData_845543C: @ 845543C
+	.incbin "sound/programmable_wave_samples/845543C.pcm"
+
+ProgrammableWaveData_845544C: @ 845544C
+	.incbin "sound/programmable_wave_samples/845544C.pcm"
