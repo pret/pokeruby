6b021036795a074e4b5da8d17cbce44e2e52e54f
camthesaxman <cameronghall@cox.net>
2016-10-27 22:47:19 -0500

    start decompiling dewford_trend

---
+++ src/dewford_trend.c
@@ -0,0 +1,31 @@
+#include "gba/gba.h"
+#include "global.h"
+
+#if 0
+
+extern u16 sub_80EB72C(u16);
+
+/*
+void sub_80FA17C(void)
+{
+    u16 i;
+    u32 r8;
+    
+    for(i = 0; i < 5; i++)
+    {
+        //gSaveBlock1.unk2DD8 = something
+    }
+}
+*/
+
+void sub_80FA5BC(void)
+{
+    
+}
+
+u16 sub_80FA648(void)
+{
+    
+}
+
+#endif
