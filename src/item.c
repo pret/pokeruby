3e866c65b54edf67f6cfcf9ddcefdf07e100e104
camthesaxman <cameronghall@cox.net>
2016-11-04 21:18:56 -0500

    start decompiling

---
+++ src/item.c
@@ -0,0 +1,12 @@
+#include "global.h"
+#include "berry.h"
+
+extern struct Berry *GetBerryInfo(u8 berry);
+
+void CopyItemName(u16 itemId)
+{
+    if(itemId == 0xAF)
+    {
+        GetBerryInfo()
+    }
+}
