2b99f7bcec8a658c502e104f735cb638047f0de7
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2016-12-16 20:39:23 +0100

    Decompile money

--- include/text.h
+++ include/text.h
@@ -4,6 +4,7 @@
 #define CHAR_SPACE          0x00
 #define CHAR_QUESTION_MARK  0xAC
 #define CHAR_HYPHEN         0xAE
+#define CHAR_CURRENCY       0xB7
 #define CHAR_COLON          0xF0
 #define EXT_CTRL_CODE_BEGIN 0xFC // extended control code
 #define PLACEHOLDER_BEGIN   0xFD // string placeholder
