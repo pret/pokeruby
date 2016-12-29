274c3864a9d370a1f5745eb489c7bf850000b6d7
ProjectRevoTPP <projectrevotpp@hotmail.com>
2016-12-29 01:26:00 +0000

    update MultichoiceStructs to use both structs

--- src/script_menu.c
+++ src/script_menu.c
@@ -3,16 +3,22 @@
 #include "text.h"
 #include "menu.h"
 
-extern void sub_80B52B4(u8);
-extern void DrawMultichoiceMenu(u8, u8, u8, u8 *, int, int);
-
 struct MultichoiceStruct
 {
 	u8 *textPtr;
 	u8 var;
 };
 
-extern struct MultichoiceStruct gMultichoiceLists[];
+struct MultichoiceListStruct
+{
+	struct MultichoiceStruct *list;
+	u8 count;
+};
+
+extern struct MultichoiceListStruct gMultichoiceLists[];
+
+extern void sub_80B52B4(u8);
+extern void DrawMultichoiceMenu(u8, u8, u8, struct MultichoiceStruct *list, int, int);
 
 extern u16 gScriptResult;
 
@@ -23,7 +29,7 @@ bool8 sub_80B5054(u8 var1, u8 var2, u8 var3, u8 var4)
 	else
 	{
 		gScriptResult = 0xFF;
-		DrawMultichoiceMenu(var1, var2, gMultichoiceLists[var3].var, gMultichoiceLists[var3].textPtr, var4, 0);
+		DrawMultichoiceMenu(var1, var2, gMultichoiceLists[var3].count, gMultichoiceLists[var3].list, var4, 0);
 		return TRUE;
 	}
 }
@@ -35,12 +41,13 @@ bool8 sub_80B50B0(u8 var1, u8 var2, u8 var3, u8 var4, u8 var5)
     else
     {
         gScriptResult = 0xFF;
-        DrawMultichoiceMenu(var1, var2, gMultichoiceLists[var3].var, gMultichoiceLists[var3].textPtr, var4, var5);
+        DrawMultichoiceMenu(var1, var2, gMultichoiceLists[var3].count, gMultichoiceLists[var3].list, var4, var5);
         return TRUE;
     }
 }
 
 u8 GetStringWidthInTilesForScriptMenu(u8 *str)
 {
+	// each tile on screen is 8x8, so it needs the number of tiles and not pixels, hence the division by 8.
     return (GetStringWidthGivenWindowConfig((struct WindowConfig *)&gWindowConfig_81E6CE4, str) + 7) / 8;
 }
