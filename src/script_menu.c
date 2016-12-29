a8edcbd87f87b8a1e8d0e3eba89c4a634202bb64
ProjectRevoTPP <projectrevotpp@hotmail.com>
2016-12-29 07:22:07 +0000

    decompile 2 more functions

--- src/script_menu.c
+++ src/script_menu.c
@@ -1,53 +1,167 @@
 #include "global.h"
 #include "task.h"
-#include "text.h"
 #include "menu.h"
-
-struct MultichoiceStruct
-{
-	u8 *textPtr;
-	u8 var;
-};
 
 struct MultichoiceListStruct
 {
-	struct MultichoiceStruct *list;
-	u8 count;
+    struct MenuAction *list;
+    u8 count;
 };
 
 extern struct MultichoiceListStruct gMultichoiceLists[];
 
+extern void sub_80B5230(u8, u8, u8, u8, u8, u8);
 extern void sub_80B52B4(u8);
-extern void DrawMultichoiceMenu(u8, u8, u8, struct MultichoiceStruct *list, int, int);
 
 extern u16 gScriptResult;
 
-bool8 sub_80B5054(u8 var1, u8 var2, u8 var3, u8 var4)
-{
-	if(FuncIsActiveTask(sub_80B52B4) == 1)
-		return FALSE;
-	else
-	{
-		gScriptResult = 0xFF;
-		DrawMultichoiceMenu(var1, var2, gMultichoiceLists[var3].count, gMultichoiceLists[var3].list, var4, 0);
-		return TRUE;
-	}
-}
+void DrawMultichoiceMenu(u8, u8, u8, struct MenuAction *list, u8, u8);
 
-bool8 sub_80B50B0(u8 var1, u8 var2, u8 var3, u8 var4, u8 var5)
+bool8 sub_80B5054(u8 left, u8 top, u8 var3, u8 var4)
 {
     if(FuncIsActiveTask(sub_80B52B4) == 1)
         return FALSE;
     else
     {
         gScriptResult = 0xFF;
-        DrawMultichoiceMenu(var1, var2, gMultichoiceLists[var3].count, gMultichoiceLists[var3].list, var4, var5);
+        DrawMultichoiceMenu(left, top, gMultichoiceLists[var3].count, gMultichoiceLists[var3].list, var4, 0);
         return TRUE;
     }
 }
 
-u8 GetStringWidthInTilesForScriptMenu(u8 *str)
+bool8 sub_80B50B0(u8 left, u8 top, u8 var3, u8 var4, u8 var5)
 {
-	// each tile on screen is 8x8, so it needs the number of tiles and not pixels, hence the division by 8.
+    if(FuncIsActiveTask(sub_80B52B4) == 1)
+        return FALSE;
+    else
+    {
+        gScriptResult = 0xFF;
+        DrawMultichoiceMenu(left, top, gMultichoiceLists[var3].count, gMultichoiceLists[var3].list, var4, var5);
+        return TRUE;
+    }
+}
+
+u16 GetStringWidthInTilesForScriptMenu(u8 *str)
+{
+    // each tile on screen is 8x8, so it needs the number of tiles and not pixels, hence the division by 8.
     return (GetStringWidthGivenWindowConfig((struct WindowConfig *)&gWindowConfig_81E6CE4, str) + 7) / 8;
 }
+
+void DrawMultichoiceMenu(u8 left, u8 top, u8 count, struct MenuAction *list, u8 var4, u8 cursorPos)
+{
+    u16 width = GetStringWidthInTilesForScriptMenu(list[0].text);
+    u16 newWidth;
+    u8 i;
+    u8 right;
+    u8 bottom;
+
+    for(i = 1; i < count; i++)
+    {
+        newWidth = GetStringWidthInTilesForScriptMenu(list[i].text);
+        if(width < newWidth)
+            width = newWidth;
+    }
+    
+    right = width;
+    right = (right + left) + 1;
+
+    if(right > 29)
+    {
+        left = left + (29 - right);
+        right = 29;
+    }
+
+    bottom = top + (2 * count + 1);
+
+    MenuDrawTextWindow(left, top, right, bottom);
+    PrintMenuItems(left + 1, top + 1, count, list);
+    InitMenu(0, left + 1, top + 1, count, cursorPos, right - left - 1);
+    sub_80B5230(left, top, right, bottom, var4, count);
+}
+
+void sub_80B5230(u8 left, u8 top, u8 right, u8 bottom, u8 unkVar, u8 count)
+{
+    u8 taskId = CreateTask(sub_80B52B4, 80);
+
+    gTasks[taskId].data[0] = left;
+    gTasks[taskId].data[1] = top;
+    gTasks[taskId].data[2] = right;
+    gTasks[taskId].data[3] = bottom;
+    gTasks[taskId].data[4] = unkVar;
+
+    if(count > 3)
+        gTasks[taskId].data[5] = TRUE;
+    else
+        gTasks[taskId].data[5] = FALSE;
+}
+
+/*
+	thumb_func_start sub_80B5230
+sub_80B5230: @ 80B5230
+	push {r4-r6,lr}
+	mov r6, r9
+	mov r5, r8
+	push {r5,r6}
+	sub sp, 0x4
+	adds r4, r0, 0
+	adds r5, r1, 0
+	adds r6, r2, 0
+	ldr r0, [sp, 0x1C]
+	mov r8, r0
+	ldr r0, [sp, 0x20]
+	mov r9, r0
+	lsls r4, 24
+	lsrs r4, 24
+	lsls r5, 24
+	lsrs r5, 24
+	lsls r6, 24
+	lsrs r6, 24
+	lsls r3, 24
+	lsrs r3, 24
+	mov r0, r8
+	lsls r0, 24
+	lsrs r0, 24
+	mov r8, r0
+	mov r0, r9
+	lsls r0, 24
+	lsrs r0, 24
+	mov r9, r0
+	ldr r0, _080B5298 @ =sub_80B52B4
+	movs r1, 0x50
+	str r3, [sp]
+	bl CreateTask
+	lsls r0, 24
+	lsrs r0, 24
+	ldr r2, _080B529C @ =gTasks
+	lsls r1, r0, 2
+	adds r1, r0
+	lsls r1, 3
+	adds r1, r2
+	strh r4, [r1, 0x8]
+	strh r5, [r1, 0xA]
+	strh r6, [r1, 0xC]
+	ldr r3, [sp]
+	strh r3, [r1, 0xE]
+	mov r0, r8
+	strh r0, [r1, 0x10]
+	mov r0, r9
+	cmp r0, 0x3
+	bls _080B52A0
+	movs r0, 0x1
+	b _080B52A2
+	.align 2, 0
+_080B5298: .4byte sub_80B52B4
+_080B529C: .4byte gTasks
+_080B52A0:
+	movs r0, 0
+_080B52A2:
+	strh r0, [r1, 0x12]
+	add sp, 0x4
+	pop {r3,r4}
+	mov r8, r3
+	mov r9, r4
+	pop {r4-r6}
+	pop {r0}
+	bx r0
+	thumb_func_end sub_80B5230
+*/
