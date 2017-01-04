19f0d5a5cfc018932fac50eb7cf430297bd3f2da
YamaArashi <shadow962@live.com>
2017-01-03 21:07:08 -0800

    make PokemonSummaryScreen_PrintTrainerMemo match

--- include/text.h
+++ include/text.h
@@ -8,6 +8,7 @@
 #define CHAR_COLON          0xF0
 #define EXT_CTRL_CODE_BEGIN 0xFC // extended control code
 #define PLACEHOLDER_BEGIN   0xFD // string placeholder
+#define CHAR_NEWLINE        0xFE
 #define EOS                 0xFF // end of string
 
 struct WindowConfig
