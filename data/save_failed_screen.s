850bdd76ac7c1727c22ed8b64c6657c20e24f8f4
YamaArashi <shadow962@live.com>
2017-01-07 18:35:24 -0800

    save failed screen incbins

--- data/save_failed_screen.s
+++ data/save_failed_screen.s
@@ -3,12 +3,6 @@
 
 	.section .rodata
 
-gUnknown_08411940:: @ 8411940
-	.incbin "baserom.gba", 0x00411940, 0x8
-
-gUnknown_08411948:: @ 8411948
-	.incbin "baserom.gba", 0x00411948, 0x18
-
 gSaveFailedClockPal:: @ 8411960
 	.incbin "graphics/misc/clock_small.gbapal"
 
