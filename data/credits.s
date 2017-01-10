0e96421b07bf5e51a7197535262950c45391cf1f
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2017-01-10 21:08:59 +0100

    Continue decompiling credits

--- data/credits.s
+++ data/credits.s
@@ -29,34 +29,66 @@ gUnknown_0840B878::
 gUnknown_0840CA00:: @ 840CA00
 	.incbin "baserom.gba", 0x0040ca00, 0x8
 
-Unknown_840CA08:
-	.incbin "baserom.gba", 0x0040ca08, 0x14
-Unknown_840CA1C:
-	.incbin "baserom.gba", 0x0040ca1c, 0x14
-Unknown_840CA30:
-	.incbin "baserom.gba", 0x0040ca30, 0x10
-Unknown_840CA40:
-	.incbin "baserom.gba", 0x0040ca40, 0x14
+
+gSpriteAnim_840CA08:
+	obj_image_anim_frame 0, 8
+	obj_image_anim_frame 64, 8
+	obj_image_anim_frame 128, 8
+	obj_image_anim_frame 192, 8
+	obj_image_anim_jump 0
+
+gSpriteAnim_840CA1C:
+	obj_image_anim_frame 0, 4
+	obj_image_anim_frame 64, 4
+	obj_image_anim_frame 128, 4
+	obj_image_anim_frame 192, 4
+	obj_image_anim_jump 0
+
+gSpriteAnim_840CA30:
+	obj_image_anim_frame 256, 4
+	obj_image_anim_frame 320, 4
+	obj_image_anim_frame 384, 4
+	obj_image_anim_end
+
+gSpriteAnim_840CA40:
+	obj_image_anim_frame 384, 30
+	obj_image_anim_frame 320, 30
+	obj_image_anim_frame 256, 30
+	obj_image_anim_frame 256, 30
+	obj_image_anim_end
 
 	.align 2
-gUnknown_0840CA54:: @ 840CA54
-	.4byte Unknown_840CA08
-	.4byte Unknown_840CA1C
-	.4byte Unknown_840CA30
-	.4byte Unknown_840CA40
+gSpriteAnimTable_0840CA54:: @ 840CA54
+	.4byte gSpriteAnim_840CA08
+	.4byte gSpriteAnim_840CA1C
+	.4byte gSpriteAnim_840CA30
+	.4byte gSpriteAnim_840CA40
 
-Unknown_840CA64:
-	.incbin "baserom.gba", 0x0040ca64, 0x14
-Unknown_840CA78:
-	.incbin "baserom.gba", 0x0040ca78, 0x14
-Unknown_840CA8C:
-	.incbin "baserom.gba", 0x0040ca8c, 0x8
+
+gSpriteAnim_840CA64:
+	obj_image_anim_frame 0, 8
+	obj_image_anim_frame 64, 8
+	obj_image_anim_frame 128, 8
+	obj_image_anim_frame 192, 8
+	obj_image_anim_jump 0
+
+gSpriteAnim_840CA78:
+	obj_image_anim_frame 0, 4
+	obj_image_anim_frame 64, 4
+	obj_image_anim_frame 128, 4
+	obj_image_anim_frame 192, 4
+	obj_image_anim_jump 0
+
+gSpriteAnim_840CA8C:
+	obj_image_anim_frame 0, 4
+	obj_image_anim_end
 
 	.align 2
-gUnknown_0840CA94:: @ 840CA94
-	.4byte Unknown_840CA64
-	.4byte Unknown_840CA78
-	.4byte Unknown_840CA8C
+gSpriteAnimTable_0840CA94:: @ 840CA94
+	.4byte gSpriteAnim_840CA64
+	.4byte gSpriteAnim_840CA78
+	.4byte gSpriteAnim_840CA8C
+
 
 gUnknown_0840CAA0:: @ 840CAA0
 	obj_tiles gHallOfFame, 6144, 1001
@@ -95,4 +127,4 @@ gSpriteAnimTable_840CAE0:: @ 840CAE0
 
 	.align 2
 gSpriteTemplate_840CAEC:: @ 840CAEC
-	spr_template 1001, 1001, gOamData_840CAC0, gSpriteAnimTable_840CAE0, NULL, gDummySpriteAffineAnimTable, sub_814580C
+	spr_template 1001, 1001, gOamData_840CAC0, gSpriteAnimTable_840CAE0, NULL, gDummySpriteAffineAnimTable, spritecb_814580C
