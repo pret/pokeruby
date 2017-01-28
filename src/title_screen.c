a3c712dc0c026fcb0b91441cfe1ba8f83e329ab4
camthesaxman <cameronghall@cox.net>
2017-01-28 01:16:14 -0600

    define data in title_screen.c

--- src/title_screen.c
+++ src/title_screen.c
@@ -19,32 +19,277 @@ extern struct MusicPlayerInfo gMPlay_BGM;
 extern u8 gUnknown_0202F7E4;
 extern u16 gUnknown_030041B4;
 extern u16 gUnknown_030042C0;
-extern u8 gUnknown_08393210[];
-extern u8 gUnknown_08393250[];
-extern u8 gUnknown_083939EC[];
-extern u8 gUnknown_08393BF8[];
-extern u16 gUnknown_08393E64[];
-extern struct SpriteTemplate gSpriteTemplate_8393ECC;
-extern struct SpriteTemplate gSpriteTemplate_8393EE4;
-extern struct SpriteSheet gUnknown_08393EFC;
-extern struct SpriteTemplate gSpriteTemplate_8393F74;
-extern struct SpriteSheet gUnknown_08393F8C;
-extern const struct SpritePalette gUnknown_08393F9C[];
-extern struct SpriteTemplate gSpriteTemplate_8393FC0;
-extern struct SpriteSheet gUnknown_08393FD8;
-extern u8 gUnknown_08E9D8CC[];
-extern u8 gUnknown_08E9F624[];
-extern u8 gUnknown_08E9F7E4[];
+extern const u8 gUnknown_08393250[];
+extern const u8 gUnknown_083939EC[];
+extern const u8 gUnknown_08393BF8[];
+extern const u16 gUnknown_08393E64[];
+extern const struct SpriteTemplate gSpriteTemplate_8393ECC;
+extern const struct SpriteTemplate gSpriteTemplate_8393EE4;
+extern const struct SpriteTemplate gSpriteTemplate_8393F74;
+extern const u8 gUnknown_08E9D8CC[];
+extern const u8 gUnknown_08E9F624[];
+extern const u8 gUnknown_08E9F7E4[];
+extern const u8 gVersionTiles[];
+extern const u8 gTitleScreenPressStart_Gfx[];
+extern const u16 gTitleScreenLogoShinePalette[];
 
-static void MainCB2(void);
-static void Task_TitleScreenPhase1(u8);
-static void Task_TitleScreenPhase2(u8);
-static void Task_TitleScreenPhase3(u8);
-static void CB2_GoToMainMenu(void);
-static void CB2_GoToClearSaveDataScreen(void);
-static void CB2_GoToResetRtcScreen(void);
-static void CB2_GoToCopyrightScreen(void);
-static void UpdateLegendaryMarkingColor(u8);
+#ifdef SAPPHIRE
+const u16 gUnknown_08393210[][16] =
+{
+    INCBIN_U16("graphics/title_screen/kyogre_dark.gbapal"),
+    INCBIN_U16("graphics/title_screen/kyogre_glow.gbapal"),
+};
+const u8 gUnknown_08393250[] = INCBIN_U8("graphics/title_screen/kyogre.4bpp.lz");
+const u8 gUnknown_083939EC[] = INCBIN_U8("graphics/title_screen/kyogre_map.bin.lz");
+const u8 gUnknown_08393BF8[] = INCBIN_U8("graphics/title_screen/water_map.bin.lz");
+#else
+const u16 gUnknown_08393210[][16] =
+{
+    INCBIN_U16("graphics/title_screen/groudon_dark.gbapal"),
+    INCBIN_U16("graphics/title_screen/groudon_glow.gbapal"),
+};
+const u8 gUnknown_08393250[] = INCBIN_U8("graphics/title_screen/groudon.4bpp.lz");
+const u8 gUnknown_083939EC[] = INCBIN_U8("graphics/title_screen/groudon_map.bin.lz");
+const u8 gUnknown_08393BF8[] = INCBIN_U8("graphics/title_screen/lava_map.bin.lz");
+#endif
+const u8 LogoShineTiles[] = INCBIN_U8("graphics/title_screen/logo_shine.4bpp.lz");
+const u16 gUnknown_08393E64[] =
+{
+    0x10,
+    0x110,
+    0x210,
+    0x310,
+    0x410,
+    0x510,
+    0x610,
+    0x710,
+    0x810,
+    0x910,
+    0xA10,
+    0xB10,
+    0xC10,
+    0xD10,
+    0xE10,
+    0xF10,
+    0x100F,
+    0x100E,
+    0x100D,
+    0x100C,
+    0x100B,
+    0x100A,
+    0x1009,
+    0x1008,
+    0x1007,
+    0x1006,
+    0x1005,
+    0x1004,
+    0x1003,
+    0x1002,
+    0x1001,
+    0x1000,
+};
+const struct OamData gOamData_8393EA4 =
+{
+    .y = 160,
+    .affineMode = 0,
+    .objMode = 0,
+    .mosaic = 0,
+    .bpp = 1,
+    .shape = 1,
+    .x = 0,
+    .matrixNum = 0,
+    .size = 3,
+    .tileNum = 0,
+    .priority = 0,
+    .paletteNum = 0,
+    .affineParam = 0,
+};
+const struct OamData gOamData_8393EAC =
+{
+    .y = 160,
+    .affineMode = 0,
+    .objMode = 0,
+    .mosaic = 0,
+    .bpp = 1,
+    .shape = 1,
+    .x = 0,
+    .matrixNum = 0,
+    .size = 3,
+    .tileNum = 0,
+    .priority = 0,
+    .paletteNum = 0,
+    .affineParam = 0,
+};
+const union AnimCmd gSpriteAnim_8393EB4[] =
+{
+    ANIMCMD_FRAME(0, 30),
+    ANIMCMD_END,
+};
+const union AnimCmd gSpriteAnim_8393EBC[] =
+{
+    ANIMCMD_FRAME(64, 30),
+    ANIMCMD_END,
+};
+const union AnimCmd *const gSpriteAnimTable_8393EC4[] =
+{
+    gSpriteAnim_8393EB4,
+};
+const union AnimCmd *const gSpriteAnimTable_8393EC8[] =
+{
+    gSpriteAnim_8393EBC,
+};
+const struct SpriteTemplate gSpriteTemplate_8393ECC =
+{
+    .tileTag = 1000,
+    .paletteTag = 1000,
+    .oam = &gOamData_8393EA4,
+    .anims = gSpriteAnimTable_8393EC4,
+    .images = NULL,
+    .affineAnims = gDummySpriteAffineAnimTable,
+    .callback = SpriteCallback_VersionBannerLeft,
+};
+const struct SpriteTemplate gSpriteTemplate_8393EE4 =
+{
+    .tileTag = 1000,
+    .paletteTag = 1000,
+    .oam = &gOamData_8393EAC,
+    .anims = gSpriteAnimTable_8393EC8,
+    .images = NULL,
+    .affineAnims = gDummySpriteAffineAnimTable,
+    .callback = SpriteCallback_VersionBannerRight,
+};
+const struct SpriteSheet gUnknown_08393EFC[] =
+{
+    {gVersionTiles, 0x1000, 1000},
+    {NULL},
+};
+const struct OamData gOamData_8393F0C =
+{
+    .y = 160,
+    .affineMode = 0,
+    .objMode = 0,
+    .mosaic = 0,
+    .bpp = 0,
+    .shape = 1,
+    .x = 0,
+    .matrixNum = 0,
+    .size = 1,
+    .tileNum = 0,
+    .priority = 0,
+    .paletteNum = 0,
+    .affineParam = 0,
+};
+const union AnimCmd gSpriteAnim_8393F14[] =
+{
+    ANIMCMD_FRAME(0, 4),
+    ANIMCMD_END,
+};
+const union AnimCmd gSpriteAnim_8393F1C[] =
+{
+    ANIMCMD_FRAME(4, 4),
+    ANIMCMD_END,
+};
+const union AnimCmd gSpriteAnim_8393F24[] =
+{
+    ANIMCMD_FRAME(8, 4),
+    ANIMCMD_END,
+};
+const union AnimCmd gSpriteAnim_8393F2C[] =
+{
+    ANIMCMD_FRAME(12, 4),
+    ANIMCMD_END,
+};
+const union AnimCmd gSpriteAnim_8393F34[] =
+{
+    ANIMCMD_FRAME(16, 4),
+    ANIMCMD_END,
+};
+const union AnimCmd gSpriteAnim_8393F3C[] =
+{
+    ANIMCMD_FRAME(20, 4),
+    ANIMCMD_END,
+};
+const union AnimCmd gSpriteAnim_8393F44[] =
+{
+    ANIMCMD_FRAME(24, 4),
+    ANIMCMD_END,
+};
+const union AnimCmd gSpriteAnim_8393F4C[] =
+{
+    ANIMCMD_FRAME(28, 4),
+    ANIMCMD_END,
+};
+const union AnimCmd *const gSpriteAnimTable_8393F54[] =
+{
+    gSpriteAnim_8393F14,
+    gSpriteAnim_8393F1C,
+    gSpriteAnim_8393F24,
+    gSpriteAnim_8393F2C,
+    gSpriteAnim_8393F34,
+    gSpriteAnim_8393F3C,
+    gSpriteAnim_8393F44,
+    gSpriteAnim_8393F4C,
+};
+const struct SpriteTemplate gSpriteTemplate_8393F74 =
+{
+    .tileTag = 1001,
+    .paletteTag = 1001,
+    .oam = &gOamData_8393F0C,
+    .anims = gSpriteAnimTable_8393F54,
+    .images = NULL,
+    .affineAnims = gDummySpriteAffineAnimTable,
+    .callback = SpriteCallback_PressStartCopyrightBanner,
+};
+const struct SpriteSheet gUnknown_08393F8C[] =
+{
+    {gTitleScreenPressStart_Gfx, 0x520, 1001},
+    {NULL},
+};
+const struct SpritePalette gUnknown_08393F9C[] =
+{
+    {gTitleScreenLogoShinePalette, 1001},
+    {NULL},
+};
+const struct OamData gOamData_8393FAC =
+{
+    .y = 160,
+    .affineMode = 0,
+    .objMode = 0,
+    .mosaic = 0,
+    .bpp = 0,
+    .shape = 0,
+    .x = 0,
+    .matrixNum = 0,
+    .size = 3,
+    .tileNum = 0,
+    .priority = 0,
+    .paletteNum = 0,
+    .affineParam = 0,
+};
+const union AnimCmd gSpriteAnim_8393FB4[] =
+{
+    ANIMCMD_FRAME(0, 4),
+    ANIMCMD_END,
+};
+const union AnimCmd *const gSpriteAnimTable_8393FBC[] =
+{
+    gSpriteAnim_8393FB4,
+};
+const struct SpriteTemplate gSpriteTemplate_8393FC0 =
+{
+    .tileTag = 1002,
+    .paletteTag = 1001,
+    .oam = &gOamData_8393FAC,
+    .anims = gSpriteAnimTable_8393FBC,
+    .images = NULL,
+    .affineAnims = gDummySpriteAffineAnimTable,
+    .callback = SpriteCallback_PokemonLogoShine,
+};
+const struct SpriteSheet gUnknown_08393FD8[] =
+{
+    {LogoShineTiles, 0x800, 1002},
+    {NULL},
+};
 
 #define _RGB(r, g, b) ((((b) & 31) << 10) + (((g) & 31) << 5) + ((r) & 31))
 
@@ -65,6 +310,16 @@ static void UpdateLegendaryMarkingColor(u8);
 #define CLEAR_SAVE_BUTTON_COMBO (B_BUTTON | SELECT_BUTTON | DPAD_UP)
 #define RESET_RTC_BUTTON_COMBO (B_BUTTON | SELECT_BUTTON | DPAD_LEFT)
 #define A_B_START_SELECT (A_BUTTON | B_BUTTON | START_BUTTON | SELECT_BUTTON)
+
+static void MainCB2(void);
+static void Task_TitleScreenPhase1(u8);
+static void Task_TitleScreenPhase2(u8);
+static void Task_TitleScreenPhase3(u8);
+static void CB2_GoToMainMenu(void);
+static void CB2_GoToClearSaveDataScreen(void);
+static void CB2_GoToResetRtcScreen(void);
+static void CB2_GoToCopyrightScreen(void);
+static void UpdateLegendaryMarkingColor(u8);
 
 void SpriteCallback_VersionBannerLeft(struct Sprite *sprite)
 {
@@ -254,9 +509,9 @@ void CB2_InitTitleScreen(void)
         ResetSpriteData();
         FreeAllSpritePalettes();
         gReservedSpritePaletteCount = 14;
-        LoadCompressedObjectPic(&gUnknown_08393EFC);
-        LoadCompressedObjectPic(&gUnknown_08393F8C);
-        LoadCompressedObjectPic(&gUnknown_08393FD8);
+        LoadCompressedObjectPic(&gUnknown_08393EFC[0]);
+        LoadCompressedObjectPic(&gUnknown_08393F8C[0]);
+        LoadCompressedObjectPic(&gUnknown_08393FD8[0]);
         LoadPalette(gUnknown_08E9F624, 0x100, 0x1C0);
         LoadSpritePalette(&gUnknown_08393F9C[0]);
         gMain.state = 2;
