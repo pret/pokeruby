823496db7694c7098c5dedc94445b26ca5c6cc91
ProjectRevoTPP <projectrevotpp@hotmail.com>
2017-03-18 03:52:30 +0100

    move diploma data to C

--- data/diploma.s
+++ data/diploma.s
@@ -3,15 +3,3 @@
 
 	.section .rodata
 
-	.align 2
-gDiplomaPalettes:: @ 840CB0C
-	.incbin "graphics/misc/diploma_national.gbapal"
-	.incbin "graphics/misc/diploma_hoenn.gbapal"
-
-	.align 2
-gDiplomaTilemap:: @ 840CB4C
-	.incbin "graphics/misc/diploma_map.bin.lz"
-
-	.align 2
-gDiplomaTiles:: @ 840D288
-	.incbin "graphics/misc/diploma.4bpp.lz"
