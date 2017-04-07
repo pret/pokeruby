ff2f6e118aa92e054b59746102f5c0400c5872d4
ProjectRevoTPP <projectrevotpp@hotmail.com>
2017-04-07 06:34:46 +0100

    split rom_810CBB4 data

---
+++ data/time_events.s
@@ -0,0 +1,31 @@
+	.include "asm/macros.inc"
+	.include "constants/constants.inc"
+
+	.section .rodata
+
+	.align 2
+gUnknown_083F8340:: @ 83F8340
+	.byte    1
+	.byte    1
+	.byte    1
+	.byte    0
+	.byte    0
+	.byte    0
+	.byte    0
+	.byte    0
+	.byte    0
+	.byte    1
+	.byte    1
+	.byte    1
+	.byte    1
+	.byte    1
+	.byte    1
+	.byte    0
+	.byte    0
+	.byte    0
+	.byte    0
+	.byte    0
+	.byte    0
+	.byte    1
+	.byte    1
+	.byte    1
