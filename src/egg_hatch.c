f13acc2ea295274e43bf3b3e73b53b266ba6a7d2
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2017-05-06 23:53:14 +0200

    Decompile CreatedHatchedMon

---
+++ src/egg_hatch.c
@@ -0,0 +1,52 @@
+#include "global.h"
+#include "pokemon.h"
+
+void CreatedHatchedMon(struct Pokemon *egg, struct Pokemon *temp) {
+    u16 species;
+    u32 personality, pokerus;
+    u8 i, friendship, language, gameMet, markings;
+    u16 moves[4];
+    u32 ivs[6];
+
+
+    species = GetMonData(egg, MON_DATA_SPECIES);
+
+    for (i = 0; i < 4; i++)
+    {
+        moves[i] = GetMonData(egg, MON_DATA_MOVE1 + i);
+    }
+
+    personality = GetMonData(egg, MON_DATA_PERSONALITY);
+
+    for (i = 0; i < 6; i++)
+    {
+        ivs[i] = GetMonData(egg, MON_DATA_HP_IV + i);
+    }
+
+    gameMet = GetMonData(egg, MON_DATA_MET_GAME);
+    markings = GetMonData(egg, MON_DATA_MARKINGS);
+    pokerus = GetMonData(egg, MON_DATA_POKERUS);
+
+    CreateMon(temp, species, 5, 32, TRUE, personality, 0, 0);
+
+    for (i = 0; i < 4; i++)
+    {
+        SetMonData(temp, MON_DATA_MOVE1 + i, (const u8 *) &moves[i]);
+    }
+
+    for (i = 0; i < 6; i++)
+    {
+        SetMonData(temp, MON_DATA_HP_IV + i, (const u8 *) &ivs[i]);
+    }
+
+    language = GAME_LANGUAGE;
+    SetMonData(temp, MON_DATA_LANGUAGE, &language);
+    SetMonData(temp, MON_DATA_MET_GAME, &gameMet);
+    SetMonData(temp, MON_DATA_MARKINGS, &markings);
+
+    friendship = 120;
+    SetMonData(temp, MON_DATA_FRIENDSHIP, &friendship);
+    SetMonData(temp, MON_DATA_POKERUS, (const u8 *) &pokerus);
+
+    *egg = *temp;
+}
