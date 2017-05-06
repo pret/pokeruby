11156a09cf23a1e06872a780795f9faa5517c435
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2017-05-06 21:16:42 +0200

    Declare `StringLengthN` conditionally in header

--- include/string_util.h
+++ include/string_util.h
@@ -16,6 +16,9 @@ u8 *StringAppend(u8 *dest, const u8 *src);
 u8 *StringCopyN(u8 *dest, const u8 *src, u8 n);
 u8 *StringAppendN(u8 *dest, const u8 *src, u8 n);
 u16 StringLength(const u8 *str);
+#ifdef GERMAN
+s32 StringLengthN(const u8 *str, s32 n);
+#endif
 s32 StringCompare(const u8 *str1, const u8 *str2);
 s32 StringCompareN(const u8 *str1, const u8 *str2, u32 n);
 u8 *ConvertIntToDecimalStringN(u8 *dest, s32 value, enum StringConvertMode mode, u8 n);
