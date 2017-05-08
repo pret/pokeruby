cf16922e1bf06f5ff85372b0decc5ddb41ee6350
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2017-05-08 22:23:56 +0200

    Decompile ShowLinkBattleRecords

---
+++ src/battle_records.c
@@ -0,0 +1,27 @@
+#include "global.h"
+#include "menu.h"
+
+extern const u8 gOtherText_BattleResults[];
+extern const u8 gOtherText_WinLoseDraw[];
+
+void PrintLinkBattleWinsLossesDraws(void *);
+
+void PrintLinkBattleRecord(void *, u8);
+
+void ShowLinkBattleRecords(void) {
+    s32 i;
+    MenuDrawTextWindow(1, 0, 28, 18);
+    sub_8072BD8((u8 *) gOtherText_BattleResults, 0, 1, 240);
+
+    PrintLinkBattleWinsLossesDraws(gSaveBlock1.linkBattleRecords);
+#if ENGLISH
+    MenuPrint(gOtherText_WinLoseDraw, 12, 6);
+#elif GERMAN
+    MenuPrint_PixelCoords((u8 *) gOtherText_WinLoseDraw, 88, 48, 1);
+#endif
+
+    for (i = 0; i < 5; i++)
+    {
+        PrintLinkBattleRecord(gSaveBlock1.linkBattleRecords[i], 6 + (i + 1) * 2);
+    }
+}
