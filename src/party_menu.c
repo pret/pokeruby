7801f9cd2e1471cd33e33047f049bbdc6da6e785
camthesaxman <cameronghall@cox.net>
2017-05-17 01:36:12 -0500

    fix up party_menu.c and decompile some more functions

--- src/party_menu.c
+++ src/party_menu.c
@@ -5,46 +5,99 @@
 #include "sound.h"
 #include "string_util.h"
 #include "task.h"
+#include "sprite.h"
+#include "palette.h"
+#include "event_data.h"
+#include "main.h"
 
 #define DATA_COUNT (6)
 
 struct Unk201C000
 {
     /*0x00*/ struct Pokemon *pokemon;
-    /*0x04*/ u8 var04;
-    /*0x05*/ u8 var05;
-    /*0x06*/ u16 var06;
-    u8 pad_08[10];
-    /*0x12*/ u16 var12;
+    /*0x04*/ u8 unk4;
+    /*0x05*/ u8 unk5;
+    /*0x06*/ u16 unk6;
+    /*0x08*/ u8 pad_08[4];
+    /*0x0C*/ s32 var0C;
+    /*0x10*/ TaskFunc unk10;
 };
 
-struct Unk201B260
+struct Unk201B000
 {
-    /*0x00*/ u8 var00;
-    /*0x01*/ u8 var01;
-    u8 pad_02[2];
-    /*0x04*/ s16 var04[DATA_COUNT * 2];
-    u8 pad_1C[2];
-    /*0x1E*/ u16 var1E;
-    /*0x20*/ u16 var20;
+    u8 filler0[0x260];
+    u8 unk260;
+    u8 unk261;
+    u8 filler262[2];
+    s16 unk264[DATA_COUNT * 2];
+    u8 filler27C[2];
+    s16 unk27E;
+    s16 unk280;
+    u16 unk282;
 };
 
-extern struct Unk201B260 unk_201B260;
-extern struct Unk201C000 unk_201C000;
+struct UnknownStruct5
+{
+    u8 unk0;
+    u8 unk1;
+    u16 *unk4;
+};
+
+extern u8 ewram[];
+#define ewram1B000 (*(struct Unk201B000 *)(ewram + 0x1B000))
+#define ewram1C000 (*(struct Unk201C000 *)(ewram + 0x1C000))
+
 extern u8 gUnknown_0202E8F4;
 extern u8 gUnknown_0202E8F6;
+extern u8 gUnknown_0202E8FA;
+extern u8 gLastFieldPokeMenuOpened;
 
-extern u8 gUnknown_083769A8[][12];
+//extern const u16 gUnknown_083769A8[][6];
+//extern const u8 gUnknown_083769A8[][12];
+extern const u8 gUnknown_083769A8[];
 extern u8 gUnknown_08376D1C[DATA_COUNT];
 extern u8 *gUnknown_08376D04[DATA_COUNT];
+extern void (*const gUnknown_08376B54[])(u8);
+extern const struct UnknownStruct5 gUnknown_08376BB4[][6];
 extern u8 gOtherText_TallPlusAndRightArrow[];
+extern u8 gOtherText_WontHaveAnyEffect[];
+extern const u8 gMoveNames[][13];
+extern u8 *const gItemEffectTable[];
 
-void task_pc_turn_off(u8 *u8, int i);
+extern bool8 IsDoubleBattle(void);
+extern void AddBagItem();
+extern void RemoveBagItem();
+extern void SetUpBattlePokemonMenu(u8);
+extern void sub_808B0C0(u8);
+
+void task_pc_turn_off();
 static void sub_806E884(u8 taskId);
 void sub_806F8AC(u8 taskId);
+void sub_806FB0C(u8 taskId);
 void PartyMenuUpdateLevelOrStatus(struct Pokemon *, u8);
-u8 ExecuteTableBasedItemEffect__(u8 u8, u16 u16, int i);
+u8 ExecuteTableBasedItemEffect__(u8, u16, u8);
 void sub_80701DC(u8 taskId);
+void DoRecoverPP(u8);
+void sub_806D538();
+void sub_806D5A4(void);
+void sub_806E8D0();
+void GetMedicineItemEffectMessage();
+
+/*
+void sub_806AEDC(void)
+{
+    const struct UnknownStruct5 *r5;
+    s32 i;
+
+    AnimateSprites();
+    BuildOamBuffer();
+    r5 = gUnknown_08376BB4[gUnknown_0202E8FA];
+    for (i = 0; i < 6; i++)
+        sub_800142C(r5[i].unk0 * 8, r5[i].unk1 * 8, r5[i].unk4, 0, (i << 5) | 0x200);
+    RunTasks();
+    UpdatePaletteFade();
+}
+*/
 
 u8 sub_806E834(u8 *message, u8 arg1)
 {
@@ -80,208 +133,194 @@ static void sub_806E884(u8 taskId)
 
 asm(".section .text_b");
 
-#ifdef NONMATCHING
 void sub_8070088(u8 taskId)
 {
-    u8 dummyTaskId;
-    struct Task *task2;
+    u8 taskId2;
 
     gTasks[taskId].func = TaskDummy;
-
-    if (GetMonData(&gPlayerParty[unk_201C000.var04], MON_DATA_SPECIES) == 0)
+    if (GetMonData(&gPlayerParty[ewram1C000.unk5], MON_DATA_SPECIES) == 0)
     {
         gTasks[taskId].func = sub_80701DC;
-        return;
     }
-
-    dummyTaskId = CreateTask(TaskDummy, 5);
-    task2 = &gTasks[dummyTaskId];
-
-    task2->data[10] = GetMonData(unk_201C000.pokemon, MON_DATA_MAX_HP);
-    task2->data[11] = GetMonData(unk_201C000.pokemon, MON_DATA_HP);
-
-    if (ExecuteTableBasedItemEffect__(unk_201C000.var05, unk_201C000.var06, 0))
+    else
     {
-        DestroyTask(dummyTaskId);
-        gTasks[taskId].func = sub_80701DC;
-        return;
+        s16 *taskData;
+
+        taskId2 = CreateTask(TaskDummy, 5);
+        taskData = gTasks[taskId2].data;
+        ewram1C000.pokemon = &gPlayerParty[ewram1C000.unk5];
+        taskData[10] = GetMonData(ewram1C000.pokemon, MON_DATA_MAX_HP);
+        taskData[11] = GetMonData(ewram1C000.pokemon, MON_DATA_HP);
+        if (ExecuteTableBasedItemEffect__(ewram1C000.unk5, ewram1C000.unk6, 0))
+        {
+            DestroyTask(taskId2);
+            gTasks[taskId].func = sub_80701DC;
+        }
+        else
+        {
+            gUnknown_0202E8F4 = 1;
+            MenuZeroFillWindowRect(3, 14, 26, 19);
+            PlaySE(SE_KAIFUKU);
+            PartyMenuUpdateLevelOrStatus(ewram1C000.pokemon, ewram1C000.unk5);
+            task_pc_turn_off(&gUnknown_083769A8[IsDoubleBattle() * 12 + ewram1C000.unk5 * 2], 9);
+            ewram1B000.unk261 = 2;
+            taskData[12] = GetMonData(ewram1C000.pokemon, MON_DATA_HP) - taskData[11];
+            taskData[14] = 1;
+            ewram1B000.unk27E = 1;
+            ewram1B000.unk280 = 1;
+            ewram1B000.unk282 = taskData[11];
+            ewram1C000.var0C = -32768;
+            gTasks[taskId2].func = sub_806F8AC;
+        }
     }
-
-    gUnknown_0202E8F4 = 1;
-    MenuZeroFillWindowRect(3, 14, 26, 19);
-    PlaySE(SE_KAIFUKU);
-    PartyMenuUpdateLevelOrStatus(unk_201C000.pokemon, unk_201C000.var05);
-
-    task_pc_turn_off(&gUnknown_083769A8[IsDoubleBattle()][unk_201C000.var05], 9);
-    unk_201B260.var01 = 2; // u8
-
-    task2->data[12] = GetMonData(unk_201C000.pokemon, MON_DATA_HP) - task2->data[11];
-    task2->data[14] = 1;
-    unk_201B260.var1E = 1; // u16
-    unk_201B260.var20 = 1; // u16
-
-    unk_201C000.var12 = -0x8000;
-    task2->func = sub_806F8AC;
 }
-#else
-__attribute__((naked))
-void sub_8070088(void)
+
+void sub_80701DC(u8 taskId)
 {
-    asm(".syntax unified\n\
-    push {r4-r7,lr}\n\
-    mov r7, r10\n\
-    mov r6, r9\n\
-    mov r5, r8\n\
-    push {r5-r7}\n\
-    sub sp, 0x4\n\
-    lsls r0, 24\n\
-    lsrs r0, 24\n\
-    ldr r1, _08070120 @ =gTasks\n\
-    mov r10, r1\n\
-    lsls r1, r0, 2\n\
-    adds r1, r0\n\
-    lsls r1, 3\n\
-    mov r2, r10\n\
-    adds r7, r1, r2\n\
-    ldr r5, _08070124 @ =TaskDummy\n\
-    str r5, [r7]\n\
-    ldr r4, _08070128 @ =0x0201c000\n\
-    ldrb r0, [r4, 0x5]\n\
-    movs r1, 0x64\n\
-    mov r9, r1\n\
-    mov r2, r9\n\
-    muls r2, r0\n\
-    adds r0, r2, 0\n\
-    ldr r1, _0807012C @ =gPlayerParty\n\
-    mov r8, r1\n\
-    add r0, r8\n\
-    movs r1, 0xB\n\
-    bl GetMonData\n\
-    cmp r0, 0\n\
-    beq _08070118\n\
-    adds r0, r5, 0\n\
-    movs r1, 0x5\n\
-    bl CreateTask\n\
-    lsls r0, 24\n\
-    lsrs r5, r0, 24\n\
-    lsls r0, r5, 2\n\
-    adds r0, r5\n\
-    lsls r0, 3\n\
-    str r0, [sp]\n\
-    mov r0, r10\n\
-    adds r0, 0x8\n\
-    ldr r2, [sp]\n\
-    adds r6, r2, r0\n\
-    ldrb r0, [r4, 0x5]\n\
-    mov r1, r9\n\
-    muls r1, r0\n\
-    adds r0, r1, 0\n\
-    add r0, r8\n\
-    str r0, [r4]\n\
-    movs r1, 0x3A\n\
-    bl GetMonData\n\
-    strh r0, [r6, 0x14]\n\
-    ldr r0, [r4]\n\
-    movs r1, 0x39\n\
-    bl GetMonData\n\
-    strh r0, [r6, 0x16]\n\
-    ldrb r0, [r4, 0x5]\n\
-    ldrh r1, [r4, 0x6]\n\
-    movs r2, 0\n\
-    bl ExecuteTableBasedItemEffect__\n\
-    lsls r0, 24\n\
-    cmp r0, 0\n\
-    beq _08070134\n\
-    adds r0, r5, 0\n\
-    bl DestroyTask\n\
-_08070118:\n\
-    ldr r0, _08070130 @ =sub_80701DC\n\
-    str r0, [r7]\n\
-    b _080701B0\n\
-    .align 2, 0\n\
-_08070120: .4byte gTasks\n\
-_08070124: .4byte TaskDummy\n\
-_08070128: .4byte 0x0201c000\n\
-_0807012C: .4byte gPlayerParty\n\
-_08070130: .4byte sub_80701DC\n\
-_08070134:\n\
-    ldr r1, _080701C0 @ =gUnknown_0202E8F4\n\
-    movs r0, 0x1\n\
-    strb r0, [r1]\n\
-    movs r0, 0x3\n\
-    movs r1, 0xE\n\
-    movs r2, 0x1A\n\
-    movs r3, 0x13\n\
-    bl MenuZeroFillWindowRect\n\
-    movs r0, 0x1\n\
-    bl PlaySE\n\
-    ldr r0, [r4]\n\
-    ldrb r1, [r4, 0x5]\n\
-    bl PartyMenuUpdateLevelOrStatus\n\
-    bl IsDoubleBattle\n\
-    adds r1, r0, 0\n\
-    lsls r1, 24\n\
-    lsrs r1, 24\n\
-    lsls r0, r1, 1\n\
-    adds r0, r1\n\
-    lsls r0, 2\n\
-    ldrb r1, [r4, 0x5]\n\
-    lsls r1, 1\n\
-    adds r0, r1\n\
-    ldr r1, _080701C4 @ =gUnknown_083769A8\n\
-    adds r0, r1\n\
-    movs r1, 0x9\n\
-    bl task_pc_turn_off\n\
-    ldr r2, _080701C8 @ =0xfffff261\n\
-    adds r1, r4, r2\n\
-    movs r0, 0x2\n\
-    strb r0, [r1]\n\
-    ldr r0, [r4]\n\
-    movs r1, 0x39\n\
-    bl GetMonData\n\
-    ldrh r1, [r6, 0x16]\n\
-    subs r0, r1\n\
-    strh r0, [r6, 0x18]\n\
-    movs r0, 0x1\n\
-    strh r0, [r6, 0x1C]\n\
-    ldr r1, _080701CC @ =0xfffff27e\n\
-    adds r0, r4, r1\n\
-    movs r1, 0x1\n\
-    strh r1, [r0]\n\
-    ldr r2, _080701D0 @ =0xfffff280\n\
-    adds r0, r4, r2\n\
-    strh r1, [r0]\n\
-    ldrh r1, [r6, 0x16]\n\
-    adds r2, 0x2\n\
-    adds r0, r4, r2\n\
-    strh r1, [r0]\n\
-    ldr r0, _080701D4 @ =0xffff8000\n\
-    str r0, [r4, 0xC]\n\
-    ldr r1, [sp]\n\
-    add r1, r10\n\
-    ldr r0, _080701D8 @ =sub_806F8AC\n\
-    str r0, [r1]\n\
-_080701B0:\n\
-    add sp, 0x4\n\
-    pop {r3-r5}\n\
-    mov r8, r3\n\
-    mov r9, r4\n\
-    mov r10, r5\n\
-    pop {r4-r7}\n\
-    pop {r0}\n\
-    bx r0\n\
-    .align 2, 0\n\
-_080701C0: .4byte gUnknown_0202E8F4\n\
-_080701C4: .4byte gUnknown_083769A8\n\
-_080701C8: .4byte 0xfffff261\n\
-_080701CC: .4byte 0xfffff27e\n\
-_080701D0: .4byte 0xfffff280\n\
-_080701D4: .4byte 0xffff8000\n\
-_080701D8: .4byte sub_806F8AC\n\
-    .syntax divided\n");
+    if (ewram1B000.unk27E == 1)
+    {
+        AddBagItem(ewram1C000.unk6, 1);
+        if (GetMonData(&gPlayerParty[ewram1C000.unk5], MON_DATA_SPECIES) != 0)
+        {
+            task_pc_turn_off(&gUnknown_083769A8[IsDoubleBattle() * 12 + ewram1C000.unk5 * 2], 3);
+            ewram1B000.unk261 = 2;
+        }
+        ewram1B000.unk27E = 0;
+    }
+    ewram1C000.unk5++;
+    if (ewram1C000.unk5 == 6)
+    {
+        gUnknown_0202E8F4 = 0;
+        if (ewram1B000.unk280 == 0)
+        {
+            gTasks[taskId].func = TaskDummy;
+            sub_806E834(gOtherText_WontHaveAnyEffect, 1);
+            CreateTask(sub_806FB0C, 8);
+        }
+        else
+        {
+            RemoveBagItem(ewram1C000.unk6, 1);
+            gTasks[taskId].func = ewram1C000.unk10;
+        }
+        gLastFieldPokeMenuOpened = 0;
+    }
+    else
+    {
+        sub_8070088(taskId);
+    }
 }
-#endif
 
+void CreateItemUseMoveMenu(u8 partyMonIndex)
+{
+    u8 r6;
+    u8 i;
+
+    r6 = 0;
+    MenuDrawTextWindow(19, 10, 29, 19);
+    for (i = 0; i < 4; i++)
+    {
+        u16 move = GetMonData(&gPlayerParty[partyMonIndex], MON_DATA_MOVE1 + i);
+
+        MenuPrint(gMoveNames[move], 20, i * 2 + 11);
+        if (move != 0)
+            r6++;
+    }
+    InitMenu(0, 20, 11, r6, 0, 9);
+}
+
+void Task_HandleItemUseMoveMenuInput(u8 taskId)
+{
+    if (gMain.newKeys & DPAD_UP)
+    {
+        PlaySE(SE_SELECT);
+        MoveMenuCursor(-1);
+    }
+    else if (gMain.newKeys & DPAD_DOWN)
+    {
+        PlaySE(SE_SELECT);
+        MoveMenuCursor(1);
+    }
+    else if (gMain.newKeys & A_BUTTON)
+    {
+        gUnknown_08376B54[0](taskId);
+    }
+    else if (gMain.newKeys & B_BUTTON)
+    {
+        PlaySE(SE_SELECT);
+        gUnknown_08376B54[1](taskId);
+    }
+}
+
+void DoPPRecoveryItemEffect(u8 taskId, u16 b, TaskFunc c)
+{
+    u8 *itemEffect;
+    u8 taskId2;
+
+    if (b == 0xAF)
+        itemEffect = gSaveBlock1.enigmaBerry.itemEffect;
+    else
+        itemEffect = gItemEffectTable[b - 13];
+    gTasks[taskId].func = TaskDummy;
+    taskId2 = CreateTask(TaskDummy, 5);
+    sub_806E8D0(taskId, b, c);
+    if (!(itemEffect[4] & 0x10))
+    {
+        gTasks[taskId2].data[11] = 0;
+        DoRecoverPP(taskId2);
+    }
+    else
+    {
+        PlaySE(SE_SELECT);
+        sub_806D538(10, 3);
+        CreateItemUseMoveMenu(ewram1C000.unk5);
+        gTasks[taskId2].func = Task_HandleItemUseMoveMenuInput;
+        gMain.newKeys = 0;
+    }
+}
+
+void ItemUseMoveMenu_HandleMoveSelection(u8 taskId)
+{
+    sub_8072DEC();
+    MenuZeroFillWindowRect(19, 10, 29, 19);
+    sub_806D5A4();
+    gTasks[taskId].data[11] = GetMenuCursorPos();
+    DoRecoverPP(taskId);
+}
+
+void ItemUseMoveMenu_HandleCancel(u8 taskId)
+{
+    sub_8072DEC();
+    MenuZeroFillWindowRect(19, 10, 29, 19);
+    if (gMain.inBattle)
+        gTasks[ewram1C000.unk4].func = SetUpBattlePokemonMenu;
+    else
+        gTasks[ewram1C000.unk4].func = sub_808B0C0;
+    sub_806D538(3, 0);
+    DestroyTask(taskId);
+}
+
+void DoRecoverPP(u8 taskId)
+{
+    u16 r5 = 0;
+
+    if (ExecuteTableBasedItemEffect__(ewram1C000.unk5, ewram1C000.unk6, gTasks[taskId].data[11]))
+    {
+        gUnknown_0202E8F4 = r5;
+        PlaySE(SE_SELECT);
+        sub_806E834(gOtherText_WontHaveAnyEffect, 1);
+    }
+    else
+    {
+        gUnknown_0202E8F4 = 1;
+        PlaySE(SE_KAIFUKU);
+        RemoveBagItem(ewram1C000.unk6, 1);
+        r5 = GetMonData(ewram1C000.pokemon, MON_DATA_MOVE1 + gTasks[taskId].data[11]);
+        StringCopy(gStringVar1, gMoveNames[r5]);
+        GetMedicineItemEffectMessage(ewram1C000.unk6);
+        sub_806E834(gStringVar4, 1);
+    }
+    gTasks[taskId].func = sub_806FB0C;
+}
 
 asm(".section .text_c");
 
@@ -296,35 +335,27 @@ void sub_8070848(u8 taskId)
         u8 x;
         u8 y;
         u32 stat;
-        struct Pokemon **pokemon;
 
-        pokemon = &unk_201C000.pokemon;
-        asm("" ::: "r0");
-        stat = GetMonData(*pokemon, gUnknown_08376D1C[i]);
+        stat = GetMonData(ewram1C000.pokemon, gUnknown_08376D1C[i]);
 
-        unk_201B260.var04[i + DATA_COUNT] = stat;
-        unk_201B260.var04[i] = stat - unk_201B260.var04[i];
+        ewram1B000.unk264[i + DATA_COUNT] = stat;
+        ewram1B000.unk264[i] = stat - ewram1B000.unk264[i];
 
         x = (i / 3) * 9 + 11;
         y = ((i % 3) << 1) + 1;
 
         MenuPrint_PixelCoords(gUnknown_08376D04[i], (x + 1) * 8, y * 8, 1);
 
-
         if (i == 2)
-        {
             MenuPrint_PixelCoords(gOtherText_TallPlusAndRightArrow, (x + 6) * 8 + 6, y * 8, 0);
-        }
         else
-        {
             MenuPrint_PixelCoords(gOtherText_TallPlusAndRightArrow, (x + 6) * 8 + 6, y * 8, 1);
-        }
 
-        gStringVar1[0] = 0xFC;
+        gStringVar1[0] = EXT_CTRL_CODE_BEGIN;
         gStringVar1[1] = 0x14;
         gStringVar1[2] = 0x06;
 
-        ConvertIntToDecimalStringN(gStringVar1 + 3, unk_201B260.var04[i], 1, 2);
+        ConvertIntToDecimalStringN(gStringVar1 + 3, ewram1B000.unk264[i], 1, 2);
 
         MenuPrint_PixelCoords(gStringVar1, (x + 6) * 8 + 12, y * 8, 0);
     }
