e263329671a49fe462e1d4226a53d3ed096eb917
PikalaxALT <pikalaxalt@gmail.com>
2017-05-21 11:30:23 -0400

    sub_805CE0C and helpers

--- include/field_map_obj.h
+++ include/field_map_obj.h
@@ -6,6 +6,6 @@
 void sub_805C058(struct MapObject *mapObject, s16 a, s16 b);
 void FieldObjectSetDirection(struct MapObject *pObject, u8 unk_18);
 void MoveCoords(u8 direction, s16 *x, s16 *y);
-void meta_step(struct MapObject *pObject,  struct Sprite *pSprite,  u32 (*d8)());
+void meta_step(struct MapObject *pObject,  struct Sprite *pSprite,  u8 (*d8)());
 
 #endif // GUARD_FIELD_MAP_OBJ_H
