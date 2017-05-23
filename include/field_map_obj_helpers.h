8186d7027ce53c74663056b8a533efff4cb8b300
scnorton <scnorton@biociphers.org>
2017-05-23 19:21:07 -0400

    Another chunk of functions with similar (and, in some cases, identical) decompilation

--- include/field_map_obj_helpers.h
+++ include/field_map_obj_helpers.h
@@ -9,5 +9,7 @@ void sub_806487C(struct Sprite *sprite, bool8 invisible);
 void sub_8064990(u8, u8);
 void UnfreezeMapObject(struct MapObject *mapObject);
 void oamt_npc_ministep_reset(struct Sprite *sprite, u8 a2, u8 a3);
+void sub_806467C(struct Sprite *sprite, u8 direction);
+bool8 sub_806468C(struct Sprite *sprite);
 
 #endif
