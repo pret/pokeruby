d857f57da015325e17e8c7d08bf4a923e0f9a2dd
scnorton <scnorton@biociphers.org>
2017-05-23 15:26:25 -0400

    Some more field map object functions; asm/field_map_obj.s down to 7k lines

--- include/field_map_obj_helpers.h
+++ include/field_map_obj_helpers.h
@@ -8,5 +8,6 @@ void UnfreezeMapObjects(void);
 void sub_806487C(struct Sprite *sprite, bool8 invisible);
 void sub_8064990(u8, u8);
 void UnfreezeMapObject(struct MapObject *mapObject);
+void oamt_npc_ministep_reset(struct Sprite *sprite, u8 a2, u8 a3);
 
 #endif
