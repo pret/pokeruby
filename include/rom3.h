21ce033204113ee0d33e934ccde50a83e9d17024
DizzyEggg <jajkodizzy@wp.pl>
2017-06-19 17:08:35 +0200

    Bring include folder

--- include/rom3.h
+++ include/rom3.h
@@ -12,13 +12,13 @@ void dp01_prepare_buffer_wireless_probably(u8 a, u16, u8 *c);
 void sub_800C1A8(u8);
 void sub_800C35C(void);
 void sub_800C47C(u8);
-void dp01_build_cmdbuf_x00_a_b_0(u8 a, u8 b, u8 c);
+void EmitGetAttributes(u8 a, u8 b, u8 c);
 void dp01_build_cmdbuf_x04_4_4_4(u8 a);
 void sub_800C704(u8, u8, u8);
 void dp01_build_cmdbuf_x07_7_7_7(u8 a);
 void dp01_build_cmdbuf_x12_a_bb(u8 a, u8 b, u16 c);
 void dp01_build_cmdbuf_x1D_1D_numargs_varargs(u8, u16, u8 *);
-void dp01_build_cmdbuf_x2E_a(u8 a, u8 b);
+void EmitBattleIntroSlide(u8 a, u8 b);
 void dp01_build_cmdbuf_x2F_2F_2F_2F(u8 a);
 void dp01_build_cmdbuf_x30_TODO(u8 a, u8 *b, u8 c);
 
