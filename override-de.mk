838c6068daf5120a36ac18245b00172bf5fa2da8
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2017-06-24 02:39:23 +0200

    Add graphics overrides

---
+++ override-de.mk
@@ -0,0 +1,21 @@
+// misc.mk
+MENUGFXDIR := graphics/interface
+INTROGFXDIR := graphics/intro
+MISCGFXDIR := graphics/misc
+
+$(MENUGFXDIR)/wordgroup_frame.4bpp: $(MENUGFXDIR)/wordgroup_frame.png
+	$(GFX) $< $@ -num_tiles 76
+$(MENUGFXDIR)/check_berry.4bpp: $(MENUGFXDIR)/check_berry.png
+	$(GFX) $< $@ -num_tiles 97
+
+$(INTROGFXDIR)/copyright.4bpp: $(INTROGFXDIR)/copyright.png
+	$(GFX) $< $@ -num_tiles 47
+
+$(MISCGFXDIR)/end_copyright.4bpp: $(MISCGFXDIR)/end_copyright.png
+	$(GFX) $< $@ -num_tiles 92
+
+// tilesets.mk
+TILESETDIR := data/tilesets
+
+$(TILESETDIR)/secondary/shop/tiles.4bpp: $(TILESETDIR)/secondary/shop/tiles.png
+	$(GFX) $< $@ -num_tiles 503
