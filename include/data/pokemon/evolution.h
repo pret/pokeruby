1fd22171ace73d435834ab4468ea4df22df6a904
scnorton <scnorton@biociphers.org>
2017-06-27 15:25:42 -0400

    Split pokemon_data.c into headers for each data subtype

---
+++ include/data/pokemon/evolution.h
@@ -0,0 +1,608 @@
+//
+// Created by Scott Norton on 6/27/17.
+//
+
+#ifndef POKERUBY_EVOLUTION_H
+#define POKERUBY_EVOLUTION_H
+
+const struct EvolutionData gEvolutionTable[] = {
+    { // ??????????
+    }, {{ // Bulbasaur
+            {EVO_LEVEL, 16, SPECIES_IVYSAUR},
+        }}, {{ // Ivysaur
+            {EVO_LEVEL, 32, SPECIES_VENUSAUR},
+        }}, { // Venusaur
+    }, {{ // Charmander
+            {EVO_LEVEL, 16, SPECIES_CHARMELEON},
+        }}, {{ // Charmeleon
+            {EVO_LEVEL, 36, SPECIES_CHARIZARD},
+        }}, { // Charizard
+    }, {{ // Squirtle
+            {EVO_LEVEL, 16, SPECIES_WARTORTLE},
+        }}, {{ // Wartortle
+            {EVO_LEVEL, 36, SPECIES_BLASTOISE},
+        }}, { // Blastoise
+    }, {{ // Caterpie
+            {EVO_LEVEL, 7, SPECIES_METAPOD},
+        }}, {{ // Metapod
+            {EVO_LEVEL, 10, SPECIES_BUTTERFREE},
+        }}, { // Butterfree
+    }, {{ // Weedle
+            {EVO_LEVEL, 7, SPECIES_KAKUNA},
+        }}, {{ // Kakuna
+            {EVO_LEVEL, 10, SPECIES_BEEDRILL},
+        }}, { // Beedrill
+    }, {{ // Pidgey
+            {EVO_LEVEL, 18, SPECIES_PIDGEOTTO},
+        }}, {{ // Pidgeotto
+            {EVO_LEVEL, 36, SPECIES_PIDGEOT},
+        }}, { // Pidgeot
+    }, {{ // Rattata
+            {EVO_LEVEL, 20, SPECIES_RATICATE},
+        }}, { // Raticate
+    }, {{ // Spearow
+            {EVO_LEVEL, 20, SPECIES_FEAROW},
+        }}, { // Fearow
+    }, {{ // Ekans
+            {EVO_LEVEL, 22, SPECIES_ARBOK},
+        }}, { // Arbok
+    }, {{ // Pikachu
+            {EVO_ITEM, ITEM_THUNDER_STONE, SPECIES_RAICHU},
+        }}, { // Raichu
+    }, {{ // Sandshrew
+            {EVO_LEVEL, 22, SPECIES_SANDSLASH},
+        }}, { // Sandslash
+    }, {{ // Nidoran♀
+            {EVO_LEVEL, 16, SPECIES_NIDORINA},
+        }}, {{ // Nidorina
+            {EVO_ITEM, ITEM_MOON_STONE, SPECIES_NIDOQUEEN},
+        }}, { // Nidoqueen
+    }, {{ // Nidoran♂
+            {EVO_LEVEL, 16, SPECIES_NIDORINO},
+        }}, {{ // Nidorino
+            {EVO_ITEM, ITEM_MOON_STONE, SPECIES_NIDOKING},
+        }}, { // Nidoking
+    }, {{ // Clefairy
+            {EVO_ITEM, ITEM_MOON_STONE, SPECIES_CLEFABLE},
+        }}, { // Clefable
+    }, {{ // Vulpix
+            {EVO_ITEM, ITEM_FIRE_STONE, SPECIES_NINETALES},
+        }}, { // Ninetales
+    }, {{ // Jigglypuff
+            {EVO_ITEM, ITEM_MOON_STONE, SPECIES_WIGGLYTUFF},
+        }}, { // Wigglytuff
+    }, {{ // Zubat
+            {EVO_LEVEL, 22, SPECIES_GOLBAT},
+        }}, {{ // Golbat
+            {EVO_FRIENDSHIP, 0, SPECIES_CROBAT},
+        }}, {{ // Oddish
+            {EVO_LEVEL, 21, SPECIES_GLOOM},
+        }}, {{ // Gloom
+            {EVO_ITEM, ITEM_LEAF_STONE, SPECIES_VILEPLUME},
+            {EVO_ITEM, ITEM_SUN_STONE, SPECIES_BELLOSSOM},
+        }}, { // Vileplume
+    }, {{ // Paras
+            {EVO_LEVEL, 24, SPECIES_PARASECT},
+        }}, { // Parasect
+    }, {{ // Venonat
+            {EVO_LEVEL, 31, SPECIES_VENOMOTH},
+        }}, { // Venomoth
+    }, {{ // Diglett
+            {EVO_LEVEL, 26, SPECIES_DUGTRIO},
+        }}, { // Dugtrio
+    }, {{ // Meowth
+            {EVO_LEVEL, 28, SPECIES_PERSIAN},
+        }}, { // Persian
+    }, {{ // Psyduck
+            {EVO_LEVEL, 33, SPECIES_GOLDUCK},
+        }}, { // Golduck
+    }, {{ // Mankey
+            {EVO_LEVEL, 28, SPECIES_PRIMEAPE},
+        }}, { // Primeape
+    }, {{ // Growlithe
+            {EVO_ITEM, ITEM_FIRE_STONE, SPECIES_ARCANINE},
+        }}, { // Arcanine
+    }, {{ // Poliwag
+            {EVO_LEVEL, 25, SPECIES_POLIWHIRL},
+        }}, {{ // Poliwhirl
+            {EVO_ITEM, ITEM_WATER_STONE, SPECIES_POLIWRATH},
+            {EVO_TRADE_ITEM, ITEM_KINGS_ROCK, SPECIES_POLITOED},
+        }}, { // Poliwrath
+    }, {{ // Abra
+            {EVO_LEVEL, 16, SPECIES_KADABRA},
+        }}, {{ // Kadabra
+            {EVO_TRADE, 0, SPECIES_ALAKAZAM},
+        }}, { // Alakazam
+    }, {{ // Machop
+            {EVO_LEVEL, 28, SPECIES_MACHOKE},
+        }}, {{ // Machoke
+            {EVO_TRADE, 0, SPECIES_MACHAMP},
+        }}, { // Machamp
+    }, {{ // Bellsprout
+            {EVO_LEVEL, 21, SPECIES_WEEPINBELL},
+        }}, {{ // Weepinbell
+            {EVO_ITEM, ITEM_LEAF_STONE, SPECIES_VICTREEBEL},
+        }}, { // Victreebel
+    }, {{ // Tentacool
+            {EVO_LEVEL, 30, SPECIES_TENTACRUEL},
+        }}, { // Tentacruel
+    }, {{ // Geodude
+            {EVO_LEVEL, 25, SPECIES_GRAVELER},
+        }}, {{ // Graveler
+            {EVO_TRADE, 0, SPECIES_GOLEM},
+        }}, { // Golem
+    }, {{ // Ponyta
+            {EVO_LEVEL, 40, SPECIES_RAPIDASH},
+        }}, { // Rapidash
+    }, {{ // Slowpoke
+            {EVO_LEVEL, 37, SPECIES_SLOWBRO},
+            {EVO_TRADE_ITEM, ITEM_KINGS_ROCK, SPECIES_SLOWKING},
+        }}, { // Slowbro
+    }, {{ // Magnemite
+            {EVO_LEVEL, 30, SPECIES_MAGNETON},
+        }}, { // Magneton
+    }, { // Farfetch'd
+    }, {{ // Doduo
+            {EVO_LEVEL, 31, SPECIES_DODRIO},
+        }}, { // Dodrio
+    }, {{ // Seel
+            {EVO_LEVEL, 34, SPECIES_DEWGONG},
+        }}, { // Dewgong
+    }, {{ // Grimer
+            {EVO_LEVEL, 38, SPECIES_MUK},
+        }}, { // Muk
+    }, {{ // Shellder
+            {EVO_ITEM, ITEM_WATER_STONE, SPECIES_CLOYSTER},
+        }}, { // Cloyster
+    }, {{ // Gastly
+            {EVO_LEVEL, 25, SPECIES_HAUNTER},
+        }}, {{ // Haunter
+            {EVO_TRADE, 0, SPECIES_GENGAR},
+        }}, { // Gengar
+    }, {{ // Onix
+            {EVO_TRADE_ITEM, ITEM_METAL_COAT, SPECIES_STEELIX},
+        }}, {{ // Drowzee
+            {EVO_LEVEL, 26, SPECIES_HYPNO},
+        }}, { // Hypno
+    }, {{ // Krabby
+            {EVO_LEVEL, 28, SPECIES_KINGLER},
+        }}, { // Kingler
+    }, {{ // Voltorb
+            {EVO_LEVEL, 30, SPECIES_ELECTRODE},
+        }}, { // Electrode
+    }, {{ // Exeggcute
+            {EVO_ITEM, ITEM_LEAF_STONE, SPECIES_EXEGGUTOR},
+        }}, { // Exeggutor
+    }, {{ // Cubone
+            {EVO_LEVEL, 28, SPECIES_MAROWAK},
+        }}, { // Marowak
+    }, { // Hitmonlee
+    }, { // Hitmonchan
+    }, { // Lickitung
+    }, {{ // Koffing
+            {EVO_LEVEL, 35, SPECIES_WEEZING},
+        }}, { // Weezing
+    }, {{ // Rhyhorn
+            {EVO_LEVEL, 42, SPECIES_RHYDON},
+        }}, { // Rhydon
+    }, {{ // Chansey
+            {EVO_FRIENDSHIP, 0, SPECIES_BLISSEY},
+        }}, { // Tangela
+    }, { // Kangaskhan
+    }, {{ // Horsea
+            {EVO_LEVEL, 32, SPECIES_SEADRA},
+        }}, {{ // Seadra
+            {EVO_TRADE_ITEM, ITEM_DRAGON_SCALE, SPECIES_KINGDRA},
+        }}, {{ // Goldeen
+            {EVO_LEVEL, 33, SPECIES_SEAKING},
+        }}, { // Seaking
+    }, {{ // Staryu
+            {EVO_ITEM, ITEM_WATER_STONE, SPECIES_STARMIE},
+        }}, { // Starmie
+    }, { // Mr. mime
+    }, {{ // Scyther
+            {EVO_TRADE_ITEM, ITEM_METAL_COAT, SPECIES_SCIZOR},
+        }}, { // Jynx
+    }, { // Electabuzz
+    }, { // Magmar
+    }, { // Pinsir
+    }, { // Tauros
+    }, {{ // Magikarp
+            {EVO_LEVEL, 20, SPECIES_GYARADOS},
+        }}, { // Gyarados
+    }, { // Lapras
+    }, { // Ditto
+    }, {{ // Eevee
+            {EVO_ITEM, ITEM_THUNDER_STONE, SPECIES_JOLTEON},
+            {EVO_ITEM, ITEM_WATER_STONE, SPECIES_VAPOREON},
+            {EVO_ITEM, ITEM_FIRE_STONE, SPECIES_FLAREON},
+            {EVO_FRIENDSHIP_DAY, 0, SPECIES_ESPEON},
+            {EVO_FRIENDSHIP_NIGHT, 0, SPECIES_UMBREON},
+        }}, { // Vaporeon
+    }, { // Jolteon
+    }, { // Flareon
+    }, {{ // Porygon
+            {EVO_TRADE_ITEM, ITEM_UP_GRADE, SPECIES_PORYGON2},
+        }}, {{ // Omanyte
+            {EVO_LEVEL, 40, SPECIES_OMASTAR},
+        }}, { // Omastar
+    }, {{ // Kabuto
+            {EVO_LEVEL, 40, SPECIES_KABUTOPS},
+        }}, { // Kabutops
+    }, { // Aerodactyl
+    }, { // Snorlax
+    }, { // Articuno
+    }, { // Zapdos
+    }, { // Moltres
+    }, {{ // Dratini
+            {EVO_LEVEL, 30, SPECIES_DRAGONAIR},
+        }}, {{ // Dragonair
+            {EVO_LEVEL, 55, SPECIES_DRAGONITE},
+        }}, { // Dragonite
+    }, { // Mewtwo
+    }, { // Mew
+    }, {{ // Chikorita
+            {EVO_LEVEL, 16, SPECIES_BAYLEEF},
+        }}, {{ // Bayleef
+            {EVO_LEVEL, 32, SPECIES_MEGANIUM},
+        }}, { // Meganium
+    }, {{ // Cyndaquil
+            {EVO_LEVEL, 14, SPECIES_QUILAVA},
+        }}, {{ // Quilava
+            {EVO_LEVEL, 36, SPECIES_TYPHLOSION},
+        }}, { // Typhlosion
+    }, {{ // Totodile
+            {EVO_LEVEL, 18, SPECIES_CROCONAW},
+        }}, {{ // Croconaw
+            {EVO_LEVEL, 30, SPECIES_FERALIGATR},
+        }}, { // Feraligatr
+    }, {{ // Sentret
+            {EVO_LEVEL, 15, SPECIES_FURRET},
+        }}, { // Furret
+    }, {{ // Hoothoot
+            {EVO_LEVEL, 20, SPECIES_NOCTOWL},
+        }}, { // Noctowl
+    }, {{ // Ledyba
+            {EVO_LEVEL, 18, SPECIES_LEDIAN},
+        }}, { // Ledian
+    }, {{ // Spinarak
+            {EVO_LEVEL, 22, SPECIES_ARIADOS},
+        }}, { // Ariados
+    }, { // Crobat
+    }, {{ // Chinchou
+            {EVO_LEVEL, 27, SPECIES_LANTURN},
+        }}, { // Lanturn
+    }, {{ // Pichu
+            {EVO_FRIENDSHIP, 0, SPECIES_PIKACHU},
+        }}, {{ // Cleffa
+            {EVO_FRIENDSHIP, 0, SPECIES_CLEFAIRY},
+        }}, {{ // Igglybuff
+            {EVO_FRIENDSHIP, 0, SPECIES_JIGGLYPUFF},
+        }}, {{ // Togepi
+            {EVO_FRIENDSHIP, 0, SPECIES_TOGETIC},
+        }}, { // Togetic
+    }, {{ // Natu
+            {EVO_LEVEL, 25, SPECIES_XATU},
+        }}, { // Xatu
+    }, {{ // Mareep
+            {EVO_LEVEL, 15, SPECIES_FLAAFFY},
+        }}, {{ // Flaaffy
+            {EVO_LEVEL, 30, SPECIES_AMPHAROS},
+        }}, { // Ampharos
+    }, { // Bellossom
+    }, {{ // Marill
+            {EVO_LEVEL, 18, SPECIES_AZUMARILL},
+        }}, { // Azumarill
+    }, { // Sudowoodo
+    }, { // Politoed
+    }, {{ // Hoppip
+            {EVO_LEVEL, 18, SPECIES_SKIPLOOM},
+        }}, {{ // Skiploom
+            {EVO_LEVEL, 27, SPECIES_JUMPLUFF},
+        }}, { // Jumpluff
+    }, { // Aipom
+    }, {{ // Sunkern
+            {EVO_ITEM, ITEM_SUN_STONE, SPECIES_SUNFLORA},
+        }}, { // Sunflora
+    }, { // Yanma
+    }, {{ // Wooper
+            {EVO_LEVEL, 20, SPECIES_QUAGSIRE},
+        }}, { // Quagsire
+    }, { // Espeon
+    }, { // Umbreon
+    }, { // Murkrow
+    }, { // Slowking
+    }, { // Misdreavus
+    }, { // Unown
+    }, { // Wobbuffet
+    }, { // Girafarig
+    }, {{ // Pineco
+            {EVO_LEVEL, 31, SPECIES_FORRETRESS},
+        }}, { // Forretress
+    }, { // Dunsparce
+    }, { // Gligar
+    }, { // Steelix
+    }, {{ // Snubbull
+            {EVO_LEVEL, 23, SPECIES_GRANBULL},
+        }}, { // Granbull
+    }, { // Qwilfish
+    }, { // Scizor
+    }, { // Shuckle
+    }, { // Heracross
+    }, { // Sneasel
+    }, {{ // Teddiursa
+            {EVO_LEVEL, 30, SPECIES_URSARING},
+        }}, { // Ursaring
+    }, {{ // Slugma
+            {EVO_LEVEL, 38, SPECIES_MAGCARGO},
+        }}, { // Magcargo
+    }, {{ // Swinub
+            {EVO_LEVEL, 33, SPECIES_PILOSWINE},
+        }}, { // Piloswine
+    }, { // Corsola
+    }, {{ // Remoraid
+            {EVO_LEVEL, 25, SPECIES_OCTILLERY},
+        }}, { // Octillery
+    }, { // Delibird
+    }, { // Mantine
+    }, { // Skarmory
+    }, {{ // Houndour
+            {EVO_LEVEL, 24, SPECIES_HOUNDOOM},
+        }}, { // Houndoom
+    }, { // Kingdra
+    }, {{ // Phanpy
+            {EVO_LEVEL, 25, SPECIES_DONPHAN},
+        }}, { // Donphan
+    }, { // Porygon2
+    }, { // Stantler
+    }, { // Smeargle
+    }, {{ // Tyrogue
+            {EVO_LEVEL_ATK_LT_DEF, 20, SPECIES_HITMONCHAN},
+            {EVO_LEVEL_ATK_GT_DEF, 20, SPECIES_HITMONLEE},
+            {EVO_LEVEL_ATK_EQ_DEF, 20, SPECIES_HITMONTOP},
+        }}, { // Hitmontop
+    }, {{ // Smoochum
+            {EVO_LEVEL, 30, SPECIES_JYNX},
+        }}, {{ // Elekid
+            {EVO_LEVEL, 30, SPECIES_ELECTABUZZ},
+        }}, {{ // Magby
+            {EVO_LEVEL, 30, SPECIES_MAGMAR},
+        }}, { // Miltank
+    }, { // Blissey
+    }, { // Raikou
+    }, { // Entei
+    }, { // Suicune
+    }, {{ // Larvitar
+            {EVO_LEVEL, 30, SPECIES_PUPITAR},
+        }}, {{ // Pupitar
+            {EVO_LEVEL, 55, SPECIES_TYRANITAR},
+        }}, { // Tyranitar
+    }, { // Lugia
+    }, { // Ho-Oh
+    }, { // Celebi
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, { // ?
+    }, {{ // Treecko
+            {EVO_LEVEL, 16, SPECIES_GROVYLE},
+        }}, {{ // Grovyle
+            {EVO_LEVEL, 36, SPECIES_SCEPTILE},
+        }}, { // Sceptile
+    }, {{ // Torchic
+            {EVO_LEVEL, 16, SPECIES_COMBUSKEN},
+        }}, {{ // Combusken
+            {EVO_LEVEL, 36, SPECIES_BLAZIKEN},
+        }}, { // Blaziken
+    }, {{ // Mudkip
+            {EVO_LEVEL, 16, SPECIES_MARSHTOMP},
+        }}, {{ // Marshtomp
+            {EVO_LEVEL, 36, SPECIES_SWAMPERT},
+        }}, { // Swampert
+    }, {{ // Poochyena
+            {EVO_LEVEL, 18, SPECIES_MIGHTYENA},
+        }}, { // Mightyena
+    }, {{ // Zigzagoon
+            {EVO_LEVEL, 20, SPECIES_LINOONE},
+        }}, { // Linoone
+    }, {{ // Wurmple
+            {EVO_LEVEL_SILCOON, 7, SPECIES_SILCOON},
+            {EVO_LEVEL_CASCOON, 7, SPECIES_CASCOON},
+        }}, {{ // Silcoon
+            {EVO_LEVEL, 10, SPECIES_BEAUTIFLY},
+        }}, { // Beautifly
+    }, {{ // Cascoon
+            {EVO_LEVEL, 10, SPECIES_DUSTOX},
+        }}, { // Dustox
+    }, {{ // Lotad
+            {EVO_LEVEL, 14, SPECIES_LOMBRE},
+        }}, {{ // Lombre
+            {EVO_ITEM, ITEM_WATER_STONE, SPECIES_LUDICOLO},
+        }}, { // Ludicolo
+    }, {{ // Seedot
+            {EVO_LEVEL, 14, SPECIES_NUZLEAF},
+        }}, {{ // Nuzleaf
+            {EVO_ITEM, ITEM_LEAF_STONE, SPECIES_SHIFTRY},
+        }}, { // Shiftry
+    }, {{ // Nincada
+            {EVO_LEVEL_NINJASK, 20, SPECIES_NINJASK},
+            {EVO_LEVEL_SHEDINJA, 20, SPECIES_SHEDINJA},
+        }}, { // Ninjask
+    }, { // Shedinja
+    }, {{ // Taillow
+            {EVO_LEVEL, 22, SPECIES_SWELLOW},
+        }}, { // Swellow
+    }, {{ // Shroomish
+            {EVO_LEVEL, 23, SPECIES_BRELOOM},
+        }}, { // Breloom
+    }, { // Spinda
+    }, {{ // Wingull
+            {EVO_LEVEL, 25, SPECIES_PELIPPER},
+        }}, { // Pelipper
+    }, {{ // Surskit
+            {EVO_LEVEL, 22, SPECIES_MASQUERAIN},
+        }}, { // Masquerain
+    }, {{ // Wailmer
+            {EVO_LEVEL, 40, SPECIES_WAILORD},
+        }}, { // Wailord
+    }, {{ // Skitty
+            {EVO_ITEM, ITEM_MOON_STONE, SPECIES_DELCATTY},
+        }}, { // Delcatty
+    }, { // Kecleon
+    }, {{ // Baltoy
+            {EVO_LEVEL, 36, SPECIES_CLAYDOL},
+        }}, { // Claydol
+    }, { // Nosepass
+    }, { // Torkoal
+    }, { // Sableye
+    }, {{ // Barboach
+            {EVO_LEVEL, 30, SPECIES_WHISCASH},
+        }}, { // Whiscash
+    }, { // Luvdisc
+    }, {{ // Corphish
+            {EVO_LEVEL, 30, SPECIES_CRAWDAUNT},
+        }}, { // Crawdaunt
+    }, {{ // Feebas
+            {EVO_BEAUTY, 170, SPECIES_MILOTIC},
+        }}, { // Milotic
+    }, {{ // Carvanha
+            {EVO_LEVEL, 30, SPECIES_SHARPEDO},
+        }}, { // Sharpedo
+    }, {{ // Trapinch
+            {EVO_LEVEL, 35, SPECIES_VIBRAVA},
+        }}, {{ // Vibrava
+            {EVO_LEVEL, 45, SPECIES_FLYGON},
+        }}, { // Flygon
+    }, {{ // Makuhita
+            {EVO_LEVEL, 24, SPECIES_HARIYAMA},
+        }}, { // Hariyama
+    }, {{ // Electrike
+            {EVO_LEVEL, 26, SPECIES_MANECTRIC},
+        }}, { // Manectric
+    }, {{ // Numel
+            {EVO_LEVEL, 33, SPECIES_CAMERUPT},
+        }}, { // Camerupt
+    }, {{ // Spheal
+            {EVO_LEVEL, 32, SPECIES_SEALEO},
+        }}, {{ // Sealeo
+            {EVO_LEVEL, 44, SPECIES_WALREIN},
+        }}, { // Walrein
+    }, {{ // Cacnea
+            {EVO_LEVEL, 32, SPECIES_CACTURNE},
+        }}, { // Cacturne
+    }, {{ // Snorunt
+            {EVO_LEVEL, 42, SPECIES_GLALIE},
+        }}, { // Glalie
+    }, { // Lunatone
+    }, { // Solrock
+    }, {{ // Azurill
+            {EVO_FRIENDSHIP, 0, SPECIES_MARILL},
+        }}, {{ // Spoink
+            {EVO_LEVEL, 32, SPECIES_GRUMPIG},
+        }}, { // Grumpig
+    }, { // Plusle
+    }, { // Minun
+    }, { // Mawile
+    }, {{ // Meditite
+            {EVO_LEVEL, 37, SPECIES_MEDICHAM},
+        }}, { // Medicham
+    }, {{ // Swablu
+            {EVO_LEVEL, 35, SPECIES_ALTARIA},
+        }}, { // Altaria
+    }, {{ // Wynaut
+            {EVO_LEVEL, 15, SPECIES_WOBBUFFET},
+        }}, {{ // Duskull
+            {EVO_LEVEL, 37, SPECIES_DUSCLOPS},
+        }}, { // Dusclops
+    }, { // Roselia
+    }, {{ // Slakoth
+            {EVO_LEVEL, 18, SPECIES_VIGOROTH},
+        }}, {{ // Vigoroth
+            {EVO_LEVEL, 36, SPECIES_SLAKING},
+        }}, { // Slaking
+    }, {{ // Gulpin
+            {EVO_LEVEL, 26, SPECIES_SWALOT},
+        }}, { // Swalot
+    }, { // Tropius
+    }, {{ // Whismur
+            {EVO_LEVEL, 20, SPECIES_LOUDRED},
+        }}, {{ // Loudred
+            {EVO_LEVEL, 40, SPECIES_EXPLOUD},
+        }}, { // Exploud
+    }, {{ // Clamperl
+            {EVO_TRADE_ITEM, ITEM_DEEP_SEA_TOOTH, SPECIES_HUNTAIL},
+            {EVO_TRADE_ITEM, ITEM_DEEP_SEA_SCALE, SPECIES_GOREBYSS},
+        }}, { // Huntail
+    }, { // Gorebyss
+    }, { // Absol
+    }, {{ // Shuppet
+            {EVO_LEVEL, 37, SPECIES_BANETTE},
+        }}, { // Banette
+    }, { // Seviper
+    }, { // Zangoose
+    }, { // Relicanth
+    }, {{ // Aron
+            {EVO_LEVEL, 32, SPECIES_LAIRON},
+        }}, {{ // Lairon
+            {EVO_LEVEL, 42, SPECIES_AGGRON},
+        }}, { // Aggron
+    }, { // Castform
+    }, { // Volbeat
+    }, { // Illumise
+    }, {{ // Lileep
+            {EVO_LEVEL, 40, SPECIES_CRADILY},
+        }}, { // Cradily
+    }, {{ // Anorith
+            {EVO_LEVEL, 40, SPECIES_ARMALDO},
+        }}, { // Armaldo
+    }, {{ // Ralts
+            {EVO_LEVEL, 20, SPECIES_KIRLIA},
+        }}, {{ // Kirlia
+            {EVO_LEVEL, 30, SPECIES_GARDEVOIR},
+        }}, { // Gardevoir
+    }, {{ // Bagon
+            {EVO_LEVEL, 30, SPECIES_SHELGON},
+        }}, {{ // Shelgon
+            {EVO_LEVEL, 50, SPECIES_SALAMENCE},
+        }}, { // Salamence
+    }, {{ // Beldum
+            {EVO_LEVEL, 20, SPECIES_METANG},
+        }}, {{ // Metang
+            {EVO_LEVEL, 45, SPECIES_METAGROSS},
+        }}, { // Metagross
+    }, { // Regirock
+    }, { // Regice
+    }, { // Registeel
+    }, { // Kyogre
+    }, { // Groudon
+    }, { // Rayquaza
+    }, { // Latias
+    }, { // Latios
+    }, { // Jirachi
+    }, { // Deoxys
+    }, { // Chimecho
+    }
+};
+
+#endif //POKERUBY_EVOLUTION_H
---
+++ include/data/pokemon/nature_stats.h
@@ -0,0 +1,37 @@
+//
+// Created by Scott Norton on 6/27/17.
+//
+
+#ifndef POKERUBY_NATURE_STATS_H
+#define POKERUBY_NATURE_STATS_H
+
+const s8 gNatureStatTable[][5] = {
+    // Atk Def Spd Sp.Atk Sp.Def
+    {    0,  0,  0,     0,     0}, // Hardy
+    {    1, -1,  0,     0,     0}, // Lonely
+    {    1,  0, -1,     0,     0}, // Brave
+    {    1,  0,  0,    -1,     0}, // Adamant
+    {    1,  0,  0,     0,    -1}, // Naughty
+    {   -1,  1,  0,     0,     0}, // Bold
+    {    0,  0,  0,     0,     0}, // Docile
+    {    0,  1, -1,     0,     0}, // Relaxed
+    {    0,  1,  0,    -1,     0}, // Impish
+    {    0,  1,  0,     0,    -1}, // Lax
+    {   -1,  0,  1,     0,     0}, // Timid
+    {    0, -1,  1,     0,     0}, // Hasty
+    {    0,  0,  0,     0,     0}, // Serious
+    {    0,  0,  1,    -1,     0}, // Jolly
+    {    0,  0,  1,     0,    -1}, // Naive
+    {   -1,  0,  0,     1,     0}, // Modest
+    {    0, -1,  0,     1,     0}, // Mild
+    {    0,  0, -1,     1,     0}, // Quiet
+    {    0,  0,  0,     0,     0}, // Bashful
+    {    0,  0,  0,     1,    -1}, // Rash
+    {   -1,  0,  0,     0,     1}, // Calm
+    {    0, -1,  0,     0,     1}, // Gentle
+    {    0,  0, -1,     0,     1}, // Sassy
+    {    0,  0,  0,    -1,     1}, // Careful
+    {    0,  0,  0,     0,     0} // Quirky
+};
+
+#endif //POKERUBY_NATURE_STATS_H
