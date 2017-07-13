761140fd1b3c755c1d0d564c82166ec8a9f01ea1
drifloony <drifloony@users.noreply.github.com>
2017-07-13 00:36:26 -0700

    decompile trader

--- include/decoration_inventory.h
+++ include/decoration_inventory.h
@@ -2,9 +2,11 @@
 #define GUARD_DECORATION_INVENTORY_H
 
 void ClearDecorationInventories(void);
+s8 sub_8133F9C(u8);
 u8 sub_8133FE4(u8);
 u8 IsThereStorageSpaceForDecoration(u8);
 u8 sub_8134074(u8);
 s8 sub_81340A8(u8);
+u8 sub_8134194(u8);
 
 #endif // GUARD_DECORATION_INVENTORY_H
