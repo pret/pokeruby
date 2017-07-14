e7cac5f7b3974d8aa181016fe603fb945fc25405
camthesaxman <cameronghall@cox.net>
2017-07-13 23:20:40 -0500

    sub_80B9C6C sucks

--- src/record_mixing.c
+++ src/record_mixing.c
@@ -88,7 +88,7 @@ void RecordMixing_ReceiveExchangePacket(u32 a)
     sub_80C0514(unk_2008000.filler1004, sizeof(struct PlayerRecords), a);
     sub_80B9B1C(unk_2008000.filler1044, sizeof(struct PlayerRecords), a);
     sub_80FA4E4(unk_2008000.easyChatPairs, sizeof(struct PlayerRecords), a);
-    sub_80B9C6C(&unk_2008000.filler10AC, sizeof(struct PlayerRecords), a, unk_2008000.tvShows);
+    sub_80B9C6C((u8 *)&unk_2008000.filler10AC, sizeof(struct PlayerRecords), a, unk_2008000.tvShows);
     sub_80B9B70(unk_2008000.filler1124, sizeof(struct PlayerRecords), a);
     sub_80B9F3C(unk_2008000.filler11C8, a);
 }
@@ -436,17 +436,17 @@ void sub_80B9BC4(u8 *a, size_t b, u8 *c, u8 d, u8 e)
     struct RecordMixing_UnknownStructSub sp0;
     struct RecordMixing_UnknownStructSub *r8;
     struct RecordMixing_UnknownStructSub *r6 = (struct RecordMixing_UnknownStructSub *)(a + b * c[d * 2]);
-    
+
     {
         struct RecordMixing_UnknownStructSub *src = r6 + c[2 * d + 1];
-        
+
         memcpy(&sp0, src, 0x38);
     }
-    
+
     r8 = (struct RecordMixing_UnknownStructSub *)(a + c[e * 2] * b);
     r6 += c[d * 2 + 1];
     memcpy(r6, r8 + c[e * 2 + 1], 0x38);
-    
+
     r8 += c[e * 2 + 1];
     memcpy(r8, &sp0, 0x38);
 }
@@ -461,24 +461,34 @@ u8 sub_80B9C4C(u8 *a)
     return r2;
 }
 
+//extern const u8 gUnknown_083D02B4[];
+extern const u8 gUnknown_083D02B4[][2];
+
+#ifdef NONMATCHING
 void sub_80B9C6C(u8 *a, u32 b, u8 c, void *d)
 {
-    u8 sp0[0x58];
+    u8 r8;
+    u8 sp4[4];
+    u8 sp8[4];
+    void *spC[4];
+    u8 sp1C[4][2];
+    u8 sp24[4][2];
     u8 sp3C;
     u16 sp40 = Random();
-    u8 r8;
     u16 i;  // r3
     u16 r7;
+    u8 r1;
     struct RecordMixing_UnknownStruct *r6;
-    
+
+    //asm("":::"r8");
     SeedRng(gLinkPlayers[0].trainerId);
     r8 = GetLinkPlayerCount();
     for (i = 0; i < 4; i++)
     {
-        sp0[4 + i] = 0xFF;
-        sp0[8 + i] = 0;
-        sp0[0x1C + i * 2 + 0] = 0;
-        sp0[0x1C + i * 2 + 1] = 0;
+        sp4[i] = 0xFF;
+        sp8[i] = 0;
+        sp1C[i][0] = 0;
+        sp1C[i][1] = 0;
     }
     sp3C = 0;
     for (i = 0; i < r8; i++)
@@ -489,26 +499,508 @@ void sub_80B9C6C(u8 *a, u32 b, u8 c, void *d)
             for (r7 = 0; r7 < r6->unk70; r7++)
             {
                 if (r6->unk74[r7] == 0)
-                    sp0[0x1C + i * 2 + r7] = 1;
+                    sp1C[i][r7] = 1;
             }
         }
         //_080B9D3C
     }
     //_080B9D46
-    r7 = 0;
-    for (i = 0; i < r8; i++)
+    for (r7 = 0, i = 0; i < r8; i++)
     {
         r6 = (struct RecordMixing_UnknownStruct *)(a + b * i);
-        if (sp0[0x1C + i * 2 + 0] == 1 || sp0[0x1C + i * 2 + 1] == 1)
+        if (sp1C[i][0] == 1 || sp1C[i][1] == 1)
             sp3C++;
-        while (1)
+        if (sp1C[i][0] == 1 && sp1C[i][1] == 0)
         {
-            if (sp0[0x1C + i * 2 + 0] == 1 || sp0[0x1C + i * 2 + 1] == 0)
-            {
-                sp0[0x24] = i;
-                sp0[0x25] = 0;
-            }
+            sp24[r7][0] = i;
+            sp24[r7][1] = 0;
+            r7++;
+        }
+        else if (sp1C[i][0] == 0 && sp1C[i][1] == 1)
+        {
+            sp24[r7][0] = i;
+            sp24[r7][1] = 0;
+            r7++;
+        }
+        //else if (sp1C[i][0] == 1 + 1 && sp1C[i][1] + 1 == 1 + 1)
+        else if (sp1C[i][0] == 1 && sp1C[i][1] == 1)
+        {
+            u8 r4, r1;
+
+            sp24[r7][0] = i;
+            r4 = sub_80B9BBC((u16 *)&r6->data[0]);
+            r1 = sub_80B9BBC((u16 *)&r6->data[1]);
+
+            asm("");
+            if (r4 == 0 && r1 != 0)
+                sp24[r7][1] = 1;
+            else if ((r4 == 0 && r1 == 0) || (r4 != 0 && r1 != 0))
+                sp24[r7][1] = Random() % 2;
+            else
+                sp24[r7][1] = 0;
+            /*
+            if (r4 == 0 && r1 != 0)
+                sp24[r7][1] = 1;
+            else if ((r4 == 0 && r1 == 0) || (r4 != 0 && r1 != 0))
+                sp24[r7][1] = Random() % 2;
+            else
+                sp24[r7][1] = 0;
+            */
+
+            /*
+            if (r4 == 0 && r1 != 0)
+                sp24[r7][1] = 1;
+            else if ((r4 == 0 && r1 == 0) || (r4 != 0 && r1 != 0))
+                //sp24[r7][1] = ((Random() << 16) >> 16) % 2;
+                sp24[r7][1] = Random() % 2;
+            else
+                sp24[r7][1] = 0;
+            */
+          //_080B9E2C:
+            r7++;
         }
     }
     //_080B9E3E
+    for (i = 0; i < 4; i++)
+    {
+        r6 = (struct RecordMixing_UnknownStruct *)a + b * c;
+        spC[i] = r6;
+    }
+    r1 = sub_80B9C4C(d) % 3;
+    switch (sp3C)
+    {
+    case 2:
+        sub_80B9BC4(a, b, (u8 *)sp24, 0, 1);
+        break;
+    case 3:
+        {
+            u8 var1 = gUnknown_083D02B4[r1][0];
+            u8 var2 = gUnknown_083D02B4[r1][1];
+            sub_80B9BC4(a, b, (u8 *)sp24, var1, var2);
+        }
+        break;
+    case 4:
+        {
+            u8 *r6 = (u8 *)sp24;
+            u8 var1 = gUnknown_083D02B4[r1][0];
+            u8 var2 = gUnknown_083D02B4[r1][1];
+            sub_80B9BC4(a, b, r6, var1, var2);
+        }
+        {
+            u8 *r6 = (u8 *)sp24;
+            u8 var1 = gUnknown_083D02B4[r1][2];
+            u8 var2 = gUnknown_083D02B4[r1][3];
+            sub_80B9BC4(a, b, r6, var1, var2);
+        }
+        break;
+    }
+    /*
+    switch (sp3C)
+    {
+        u8 var1;
+        u8 var2;
+
+    case 2:
+        sub_80B9BC4(a, b, (u8 *)sp24, 0, 1);
+        break;
+    case 3:
+        var1 = gUnknown_083D02B4[r1 * 2 + 0];
+        var2 = gUnknown_083D02B4[r1 * 2 + 1];
+        sub_80B9BC4(a, b, (u8 *)sp24, var1, var2);
+        break;
+    case 4:
+        {
+            var1 = gUnknown_083D02B4[r1 * 2 + 0];
+            var2 = gUnknown_083D02B4[r1 * 2 + 1];
+            sub_80B9BC4(a, b, (u8 *)sp24, var1, var2);
+
+            var1 = gUnknown_083D02B4[r1 * 2 + 2 + 0];
+            var2 = gUnknown_083D02B4[r1 * 2 + 2 + 1];
+            sub_80B9BC4(a, b, (u8 *)sp24, var1, var2);
+        }
+        break;
+    }
+    */
+    //_080B9EF0
+    //memcpy(&gSaveBlock1.filler_303C.data[0], a + b * c, 0x38);
+    //memcpy(&gSaveBlock1.filler_303C.data[1], a + b * c + 0x38, 0x38);
+    r6 = (struct RecordMixing_UnknownStruct *)(a + b * c);
+    gSaveBlock1.filler_303C.data[0] = r6->data[0];
+    gSaveBlock1.filler_303C.data[1] = r6->data[1];
+    //memcpy(&gSaveBlock1.filler_303C.data[0], &r6->data[0], 0x38);
+    //memcpy(&gSaveBlock1.filler_303C.data[1], &r6->data[1], 0x38);
+    SeedRng(sp40);
 }
+#else
+__attribute__((naked))
+void sub_80B9C6C(u8 *a, u32 b, u8 c, void *d)
+{
+    asm(".syntax unified\n\
+    push {r4-r7,lr}\n\
+    mov r7, r10\n\
+    mov r6, r9\n\
+    mov r5, r8\n\
+    push {r5-r7}\n\
+    sub sp, 0x58\n\
+    str r0, [sp, 0x2C]\n\
+    str r1, [sp, 0x30]\n\
+    str r3, [sp, 0x38]\n\
+    lsls r2, 24\n\
+    lsrs r2, 24\n\
+    str r2, [sp, 0x34]\n\
+    bl Random\n\
+    lsls r0, 16\n\
+    lsrs r0, 16\n\
+    str r0, [sp, 0x40]\n\
+    ldr r0, _080B9DA8 @ =gLinkPlayers\n\
+    ldrh r0, [r0, 0x4]\n\
+    bl SeedRng\n\
+    bl GetLinkPlayerCount\n\
+    lsls r0, 24\n\
+    lsrs r0, 24\n\
+    mov r8, r0\n\
+    movs r3, 0\n\
+    add r0, sp, 0x1C\n\
+    mov r9, r0\n\
+    movs r1, 0x1D\n\
+    add r1, sp\n\
+    mov r10, r1\n\
+    mov r2, sp\n\
+    adds r2, 0xC\n\
+    str r2, [sp, 0x4C]\n\
+    movs r7, 0xFF\n\
+    add r4, sp, 0x8\n\
+    movs r2, 0\n\
+    mov r6, r9\n\
+    mov r5, r10\n\
+_080B9CBC:\n\
+    mov r1, sp\n\
+    adds r1, r3\n\
+    adds r1, 0x4\n\
+    ldrb r0, [r1]\n\
+    orrs r0, r7\n\
+    strb r0, [r1]\n\
+    adds r0, r4, r3\n\
+    strb r2, [r0]\n\
+    lsls r1, r3, 1\n\
+    adds r0, r6, r1\n\
+    strb r2, [r0]\n\
+    adds r1, r5, r1\n\
+    strb r2, [r1]\n\
+    adds r0, r3, 0x1\n\
+    lsls r0, 16\n\
+    lsrs r3, r0, 16\n\
+    cmp r3, 0x3\n\
+    bls _080B9CBC\n\
+    movs r4, 0\n\
+    str r4, [sp, 0x3C]\n\
+    movs r3, 0\n\
+    mov r1, r8\n\
+    lsls r0, r1, 16\n\
+    lsrs r1, r0, 16\n\
+    str r0, [sp, 0x50]\n\
+    ldr r4, [sp, 0x30]\n\
+    ldr r0, [sp, 0x34]\n\
+    adds r2, r4, 0\n\
+    muls r2, r0\n\
+    str r2, [sp, 0x48]\n\
+    ldr r2, [sp, 0x3C]\n\
+    cmp r2, r1\n\
+    bcs _080B9D46\n\
+    mov r8, r1\n\
+_080B9D00:\n\
+    ldr r4, [sp, 0x30]\n\
+    adds r0, r4, 0\n\
+    muls r0, r3\n\
+    ldr r1, [sp, 0x2C]\n\
+    adds r6, r1, r0\n\
+    ldr r0, [r6, 0x70]\n\
+    cmp r0, 0\n\
+    beq _080B9D3C\n\
+    movs r7, 0\n\
+    cmp r7, r0\n\
+    bcs _080B9D3C\n\
+    adds r4, r6, 0\n\
+    adds r4, 0x74\n\
+    mov r2, r9\n\
+    lsls r1, r3, 1\n\
+    movs r5, 0x1\n\
+_080B9D20:\n\
+    lsls r0, r7, 1\n\
+    adds r0, r4, r0\n\
+    ldrh r0, [r0]\n\
+    cmp r0, 0\n\
+    bne _080B9D30\n\
+    adds r0, r7, r1\n\
+    adds r0, r2, r0\n\
+    strb r5, [r0]\n\
+_080B9D30:\n\
+    adds r0, r7, 0x1\n\
+    lsls r0, 16\n\
+    lsrs r7, r0, 16\n\
+    ldr r0, [r6, 0x70]\n\
+    cmp r7, r0\n\
+    bcc _080B9D20\n\
+_080B9D3C:\n\
+    adds r0, r3, 0x1\n\
+    lsls r0, 16\n\
+    lsrs r3, r0, 16\n\
+    cmp r3, r8\n\
+    bcc _080B9D00\n\
+_080B9D46:\n\
+    movs r7, 0\n\
+    movs r3, 0\n\
+    ldr r2, [sp, 0x50]\n\
+    cmp r2, 0\n\
+    beq _080B9E3E\n\
+    mov r4, sp\n\
+    adds r4, 0x24\n\
+    str r4, [sp, 0x44]\n\
+    movs r0, 0x25\n\
+    add r0, sp\n\
+    mov r8, r0\n\
+_080B9D5C:\n\
+    ldr r1, [sp, 0x30]\n\
+    adds r0, r1, 0\n\
+    muls r0, r3\n\
+    ldr r2, [sp, 0x2C]\n\
+    adds r6, r2, r0\n\
+    lsls r1, r3, 1\n\
+    mov r4, r9\n\
+    adds r0, r4, r1\n\
+    ldrb r0, [r0]\n\
+    cmp r0, 0x1\n\
+    beq _080B9D7C\n\
+    mov r2, r10\n\
+    adds r0, r2, r1\n\
+    ldrb r0, [r0]\n\
+    cmp r0, 0x1\n\
+    bne _080B9D86\n\
+_080B9D7C:\n\
+    ldr r0, [sp, 0x3C]\n\
+    adds r0, 0x1\n\
+    lsls r0, 24\n\
+    lsrs r0, 24\n\
+    str r0, [sp, 0x3C]\n\
+_080B9D86:\n\
+    mov r4, r9\n\
+    adds r0, r4, r1\n\
+    ldrb r0, [r0]\n\
+    cmp r0, 0x1\n\
+    bne _080B9DAC\n\
+    mov r2, r10\n\
+    adds r0, r2, r1\n\
+    ldrb r2, [r0]\n\
+    cmp r2, 0\n\
+    bne _080B9DAC\n\
+_080B9D9A:\n\
+    lsls r1, r7, 1\n\
+    ldr r4, [sp, 0x44]\n\
+    adds r0, r4, r1\n\
+    strb r3, [r0]\n\
+    add r1, r8\n\
+    strb r2, [r1]\n\
+    b _080B9E2C\n\
+    .align 2, 0\n\
+_080B9DA8: .4byte gLinkPlayers\n\
+_080B9DAC:\n\
+    mov r2, r9\n\
+    adds r0, r2, r1\n\
+    ldrb r0, [r0]\n\
+    cmp r0, 0\n\
+    bne _080B9DC0\n\
+    mov r4, r10\n\
+    adds r0, r4, r1\n\
+    ldrb r2, [r0]\n\
+    cmp r2, 0x1\n\
+    beq _080B9D9A\n\
+_080B9DC0:\n\
+    mov r2, r9\n\
+    adds r0, r2, r1\n\
+    ldrb r0, [r0]\n\
+    cmp r0, 0x1\n\
+    bne _080B9E32\n\
+    mov r4, r10\n\
+    adds r0, r4, r1\n\
+    ldrb r0, [r0]\n\
+    cmp r0, 0x1\n\
+    bne _080B9E32\n\
+    lsls r5, r7, 1\n\
+    ldr r1, [sp, 0x44]\n\
+    adds r0, r1, r5\n\
+    strb r3, [r0]\n\
+    adds r0, r6, 0\n\
+    str r3, [sp, 0x54]\n\
+    bl sub_80B9BBC\n\
+    adds r4, r0, 0\n\
+    lsls r4, 24\n\
+    lsrs r4, 24\n\
+    adds r0, r6, 0\n\
+    adds r0, 0x38\n\
+    bl sub_80B9BBC\n\
+    lsls r0, 24\n\
+    lsrs r1, r0, 24\n\
+    ldr r3, [sp, 0x54]\n\
+    cmp r4, 0\n\
+    bne _080B9E0A\n\
+    cmp r1, 0\n\
+    beq _080B9E0E\n\
+    mov r2, r8\n\
+    adds r1, r2, r5\n\
+    movs r0, 0x1\n\
+    strb r0, [r1]\n\
+    b _080B9E2C\n\
+_080B9E0A:\n\
+    cmp r1, 0\n\
+    beq _080B9E26\n\
+_080B9E0E:\n\
+    str r3, [sp, 0x54]\n\
+    bl Random\n\
+    mov r4, r8\n\
+    adds r2, r4, r5\n\
+    lsls r0, 16\n\
+    lsrs r0, 16\n\
+    movs r1, 0x1\n\
+    ands r0, r1\n\
+    strb r0, [r2]\n\
+    ldr r3, [sp, 0x54]\n\
+    b _080B9E2C\n\
+_080B9E26:\n\
+    mov r2, r8\n\
+    adds r0, r2, r5\n\
+    strb r1, [r0]\n\
+_080B9E2C:\n\
+    adds r0, r7, 0x1\n\
+    lsls r0, 16\n\
+    lsrs r7, r0, 16\n\
+_080B9E32:\n\
+    adds r0, r3, 0x1\n\
+    lsls r0, 16\n\
+    lsrs r3, r0, 16\n\
+    ldr r4, [sp, 0x50]\n\
+    cmp r0, r4\n\
+    bcc _080B9D5C\n\
+_080B9E3E:\n\
+    movs r3, 0\n\
+    ldr r1, [sp, 0x48]\n\
+    lsls r0, r1, 4\n\
+    subs r0, r1\n\
+    lsls r0, 3\n\
+    ldr r2, [sp, 0x2C]\n\
+    adds r6, r2, r0\n\
+    ldr r1, [sp, 0x4C]\n\
+_080B9E4E:\n\
+    lsls r0, r3, 2\n\
+    adds r0, r1, r0\n\
+    str r6, [r0]\n\
+    adds r0, r3, 0x1\n\
+    lsls r0, 16\n\
+    lsrs r3, r0, 16\n\
+    cmp r3, 0x3\n\
+    bls _080B9E4E\n\
+    ldr r0, [sp, 0x38]\n\
+    bl sub_80B9C4C\n\
+    lsls r0, 24\n\
+    lsrs r0, 24\n\
+    movs r1, 0x3\n\
+    bl __umodsi3\n\
+    lsls r0, 24\n\
+    lsrs r1, r0, 24\n\
+    ldr r4, [sp, 0x3C]\n\
+    cmp r4, 0x3\n\
+    beq _080B9E9C\n\
+    cmp r4, 0x3\n\
+    bgt _080B9E82\n\
+    cmp r4, 0x2\n\
+    beq _080B9E8A\n\
+    b _080B9EF0\n\
+_080B9E82:\n\
+    ldr r0, [sp, 0x3C]\n\
+    cmp r0, 0x4\n\
+    beq _080B9EBC\n\
+    b _080B9EF0\n\
+_080B9E8A:\n\
+    add r2, sp, 0x24\n\
+    movs r0, 0x1\n\
+    str r0, [sp]\n\
+    ldr r0, [sp, 0x2C]\n\
+    ldr r1, [sp, 0x30]\n\
+    movs r3, 0\n\
+    bl sub_80B9BC4\n\
+    b _080B9EF0\n\
+_080B9E9C:\n\
+    ldr r0, _080B9EB8 @ =gUnknown_083D02B4\n\
+    lsls r1, 1\n\
+    adds r2, r1, r0\n\
+    ldrb r3, [r2]\n\
+    adds r0, 0x1\n\
+    adds r1, r0\n\
+    ldrb r0, [r1]\n\
+    add r2, sp, 0x24\n\
+    str r0, [sp]\n\
+    ldr r0, [sp, 0x2C]\n\
+    ldr r1, [sp, 0x30]\n\
+    bl sub_80B9BC4\n\
+    b _080B9EF0\n\
+    .align 2, 0\n\
+_080B9EB8: .4byte gUnknown_083D02B4\n\
+_080B9EBC:\n\
+    add r6, sp, 0x24\n\
+    ldr r4, _080B9F2C @ =gUnknown_083D02BA\n\
+    lsls r5, r1, 2\n\
+    adds r0, r5, r4\n\
+    ldrb r3, [r0]\n\
+    adds r0, r4, 0x1\n\
+    adds r0, r5, r0\n\
+    ldrb r0, [r0]\n\
+    str r0, [sp]\n\
+    ldr r0, [sp, 0x2C]\n\
+    ldr r1, [sp, 0x30]\n\
+    adds r2, r6, 0\n\
+    bl sub_80B9BC4\n\
+    adds r0, r4, 0x2\n\
+    adds r0, r5, r0\n\
+    ldrb r3, [r0]\n\
+    adds r4, 0x3\n\
+    adds r5, r4\n\
+    ldrb r0, [r5]\n\
+    str r0, [sp]\n\
+    ldr r0, [sp, 0x2C]\n\
+    ldr r1, [sp, 0x30]\n\
+    adds r2, r6, 0\n\
+    bl sub_80B9BC4\n\
+_080B9EF0:\n\
+    ldr r1, [sp, 0x2C]\n\
+    ldr r2, [sp, 0x48]\n\
+    adds r6, r1, r2\n\
+    ldr r4, _080B9F30 @ =gSaveBlock1\n\
+    ldr r1, _080B9F34 @ =0x0000303c\n\
+    adds r0, r4, r1\n\
+    adds r1, r6, 0\n\
+    movs r2, 0x38\n\
+    bl memcpy\n\
+    ldr r2, _080B9F38 @ =0x00003074\n\
+    adds r4, r2\n\
+    adds r1, r6, 0\n\
+    adds r1, 0x38\n\
+    adds r0, r4, 0\n\
+    movs r2, 0x38\n\
+    bl memcpy\n\
+    ldr r0, [sp, 0x40]\n\
+    bl SeedRng\n\
+    add sp, 0x58\n\
+    pop {r3-r5}\n\
+    mov r8, r3\n\
+    mov r9, r4\n\
+    mov r10, r5\n\
+    pop {r4-r7}\n\
+    pop {r0}\n\
+    bx r0\n\
+    .align 2, 0\n\
+_080B9F2C: .4byte gUnknown_083D02BA\n\
+_080B9F30: .4byte gSaveBlock1\n\
+_080B9F34: .4byte 0x0000303c\n\
+_080B9F38: .4byte 0x00003074\n\
+    .syntax divided\n");
+}
+#endif
