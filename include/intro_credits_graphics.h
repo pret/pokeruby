00abce98dba72f1119b41e5f7f9b186fb30dbf64
golemgalvanize <throwawaygolem@gmail.com>
2017-08-13 20:05:42 -0400

    decompile intro_credits_graphics.s (#381)
    * Update
    
    * Update
    
    * Update
    
    * update
    
    * intro_credits_graphics
    
    * decompiled up to sub_80cb438
    
    * decompiled up to sub_80CB7EC
    
    * decompiled up to sub_80cbc8c
    
    * decompiled up to sub_80CC408
    
    * decompiled up to sub_80CCD24
    
    * fixed it up a little
    
    * more fixes
    
    * Update ld_script.txt
    
    * Delete battle_anim_80CA710.c
    
    * Update battle_anim_80CA710.s
    
    reverted

--- include/intro_credits_graphics.h
+++ include/intro_credits_graphics.h
@@ -10,15 +10,16 @@ extern const struct SpritePalette gIntro2SpritePalettes[];
 extern const struct CompressedSpriteSheet gUnknown_08416E24;
 extern const struct CompressedSpriteSheet gUnknown_08416E34;
 
-void load_intro_part2_graphics(/*TODO: arg types*/);
-void sub_8148C78(/*TODO: arg types*/);
+void load_intro_part2_graphics(u8 a);
+void sub_8148C78(u8 a);
 void sub_8148CB0(u8);
 void sub_8148E90(u8);
-u8 sub_8148EC0(/*TODO: arg types*/);
-void sub_8149020(/*TODO: arg types*/);
-u8 intro_create_brendan_sprite(/*TODO: arg types*/);
-u8 intro_create_may_sprite(/*TODO: arg types*/);
-u8 intro_create_latios_sprite(/*TODO: arg types*/);
-u8 intro_create_latias_sprite(/*TODO: arg types*/);
+u8 sub_8148EC0(u8 a, u16 b, u16 c, u16 d);
+void sub_8148F3C(u8);
+void sub_8149020(u8);
+u8 intro_create_brendan_sprite(s16 a, s16 b);
+u8 intro_create_may_sprite(s16 a, s16 b);
+u8 intro_create_latios_sprite(s16 a, s16 b);
+u8 intro_create_latias_sprite(s16 a, s16 b);
 
 #endif // GUARD_INTRO_CREDITS_GRAPHICS_H
