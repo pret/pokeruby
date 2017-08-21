cc1ee24512d974814499fcc3d35166901c227c9b
Raúl Peñacoba <raul.mikaop.zelda@gmail.com>
2017-08-21 11:14:24 +0200

    sub_80EE3D8

--- src/pokenav_before.c
+++ src/pokenav_before.c
@@ -2141,3 +2141,76 @@ void sub_80EE294() {
 		break;
 	}
 }
+
+void sub_80F6F10();
+void sub_80EE58C();
+
+void sub_80EE3D8() {
+	u8 var1;
+	switch (ewram0.var304) {
+	case 0:
+		sub_80F1E84();
+		sub_80F2D04(0);
+		ewram0.var304++;
+		break;
+	case 1:
+		if (sub_80F1F10()) return;
+		sub_80EEFBC(0x9);
+		ewram0.var304++;
+		break;
+	case 2:
+		if (!(var1 = sub_80EEF34())) {
+			BeginNormalPaletteFade(ewram0.var308, -1, 0x0, 0x10, var1);
+			ewram0.var304++;
+		}
+		break;
+	case 3:
+		if (gPaletteFade.active) return;
+		SetVBlankCallback(NULL);
+		sub_80EED0C();
+		sub_80EF814();
+		sub_80EEE08();
+		ewram0.var304++;
+		break;
+	case 4:
+		sub_80F2620();
+		ewram0.var304++;
+		break;
+	case 5:
+		sub_80F0264(0x2);
+		ewram0.var304++;
+	case 6:
+		if (sub_80F02A0()) return;
+		ewram0.var304++;
+		break;
+	case 7:
+		sub_80F2C80(0x3);
+		ewram0.var304++;
+	case 8:
+		if (sub_80F2CBC(0x3)) return;
+		ewram0.var304++;
+		break;
+	case 9:
+		sub_80F3008(0x2);
+		ewram0.var304++;
+		break;
+	case 0xA:
+		if (sub_8055870()) return;
+		ewram0.var304++;
+		break;
+	case 0xB:
+		sub_80F6F10();
+		BeginNormalPaletteFade(ewram0.var308, -1, 0x10, 0, 0);
+		SetVBlankCallback(&sub_80EBD68);
+		ewram0.var304++;
+		break;
+	case 0xC:
+		sub_80EED2C(0x5);
+		ewram0.var304++;
+		break;
+	case 0xD:
+		if (gPaletteFade.active) return;
+		sub_80EBDBC(&sub_80EE58C);
+		break;
+	}
+}
