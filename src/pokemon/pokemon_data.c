91f6d8118788d18b258873a062da1318569eaf0f
camthesaxman <cameronghall@cox.net>
2017-10-26 13:43:08 -0500

    also fix ambiguous MON_DATA_SPD_EV and MON_DATA_SPD2 constants

--- src/pokemon/pokemon_data.c
+++ src/pokemon/pokemon_data.c
@@ -93,7 +93,7 @@ const u8 gUnknown_082082F2[] = {
     MON_DATA_HP_EV,
     MON_DATA_ATK_EV,
     MON_DATA_DEF_EV,
-    MON_DATA_SPD_EV,
+    MON_DATA_SPEED_EV,
     MON_DATA_SPDEF_EV,
     MON_DATA_SPATK_EV
 };
