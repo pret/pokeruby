0f0d652f4d8b0347f086ee6aab899f4b1c461839
sceptillion <33798691+sceptillion@users.noreply.github.com>
2017-11-19 22:31:29 -0800

    clean up unknown_task variables

--- include/unknown_task.h
+++ include/unknown_task.h
@@ -9,6 +9,24 @@ struct UnknownTaskStruct
     u8 unk9;
 };
 
+struct UnknownTaskStruct2
+{
+    void *src[2];
+    volatile void *dest;
+    u32 unkC;
+    void (*unk10)(void);
+    u8 srcBank;
+    u8 unk15;
+    u8 unk16;
+    u8 unk17;
+    u8 taskId;
+    u8 filler19[0x7];
+};
+
+extern struct UnknownTaskStruct2 gUnknown_03004DC0;
+
+extern u16 gUnknown_03004DE0[][0x3C0];
+
 void remove_some_task(void);
 void remove_some_task(void);
 void dp12_8087EA4(void);
