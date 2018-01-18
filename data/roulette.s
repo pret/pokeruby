bcbc9d040ba671df27183bff0b7356da3d9511f6
Cameron Hall <cameronghall@cox.net>
2018-01-18 15:19:31 -0600

    finish adding all debug code

--- data/roulette.s
+++ data/roulette.s
@@ -133,6 +133,13 @@ gUnknown_083F8EE8:: @ 83F8EE8
 gUnknown_083F8EF4:: @ 83F8EF4
 	.byte 0, 3, 4, 6, 12
 
+.if DEBUG
+@ Could also be after English-only graphics
+gUnknown_Debug_0842510D:: @ 0x842510D
+	.string "Set　COIN　and　Press　START\n"
+	.string "Aーp1　Bーm1　R:Lー×10$"
+.endif
+
 .ifdef ENGLISH
 	.align 2
 RouletteSpritePalette_01:
