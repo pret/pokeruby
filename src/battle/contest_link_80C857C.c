4e98ffed3339ca9fd7c4f0915da707aa8b2417ef
PikalaxALT <pikalaxalt@gmail.com>
2018-01-22 14:54:37 -0500

    through sub_80C8604

--- src/battle/contest_link_80C857C.c
+++ src/battle/contest_link_80C857C.c
@@ -1,5 +1,51 @@
 #include "global.h"
+#include "ewram.h"
+#include "task.h"
 #include "link.h"
+
+void sub_80C8644(u8 taskId);
+
+void sub_80C857C(const void *data, u16 size)
+{
+    memcpy(eContestLinkSendBuffer, data, size);
+    SendBlock(bitmask_all_link_players_but_self(), eContestLinkSendBuffer, size);
+}
+
+bool8 sub_80C85AC(u8 who)
+{
+    u8 flag = 1 << who;
+    if (!(GetBlockReceivedStatus() & flag))
+        return FALSE;
+    ResetBlockReceivedFlag(flag);
+    return TRUE;
+}
+
+bool8 sub_80C85D8(void)
+{
+    int i;
+
+    for (i = 0; i < MAX_LINK_PLAYERS; i++)
+    {
+        if (!((GetBlockReceivedStatus() >> i) & 1))
+            return FALSE;
+    }
+    ResetBlockReceivedFlags();
+    return TRUE;
+}
+
+void sub_80C8604(u8 taskId)
+{
+#if ENGLISH
+    u8 i;
+
+    for (i = 0; i < 4; i++)
+        gBlockRecvBuffer[i][0] = 0xff;
+#endif
+    gTasks[taskId].data[0] = 0;
+    gTasks[taskId].func = sub_80C8644;
+}
+
+asm(".section .text_de");
 
 #if GERMAN
 
