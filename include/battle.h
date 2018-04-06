90380f68bd0725eb77f38f5c00828efba89e5219
Marcus Huderle <huderlem@gmail.com>
2018-04-05 20:39:07 -0700

    Rename cute_sketch to contest_painting_effects, and battle_1 to decompress

--- include/battle.h
+++ include/battle.h
@@ -708,7 +708,7 @@ void MarkBufferBankForExecution(u8 bank);
 
 extern u8 gBattleTextBuff1[];
 
-// asm/battle_1.o
+// src/battle_bg.o
 void sub_800D6D4();
 void ApplyPlayerChosenFrameToBattleMenu();
 void DrawMainBattleBackground(void);
