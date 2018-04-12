95bf34a6d4af0eca83d9ca3bf7e9a898e00429c8
PikalaxALT <pikalaxalt@gmail.com>
2018-04-12 08:37:45 -0400

    debug_80C35DC

--- src/debug/watanabe_debug_menu.c
+++ src/debug/watanabe_debug_menu.c
@@ -1,5 +1,13 @@
 #if DEBUG
 #include "global.h"
+#include "debug.h"
+#include "palette.h"
+#include "scanline_effect.h"
+#include "main.h"
+#include "task.h"
+#include "sprite.h"
+#include "text.h"
+#include "menu.h"
 
 EWRAM_DATA u8 gUnknown_Debug_20389EC[0x20] = { 0 };
 EWRAM_DATA u8 gUnknown_Debug_2038A0C[0x10] = { 0 };
@@ -7,5 +15,44 @@ EWRAM_DATA u8 gUnknown_Debug_2038A1C[4] = { 0 };
 EWRAM_DATA u8 gUnknown_Debug_2038A20[4] = { 0 };
 
 u8 byte_3005E30[0x20];
+
+extern const struct SpriteSheet stru_83F8828[2];
+extern const struct SpritePalette stru_83F8838[2];
+
+void debug_69(struct Sprite *sprite)
+{
+
+}
+
+void debug_80C35DC(void)
+{
+    SetVBlankCallback(NULL);
+    REG_DISPCNT = 0;
+    REG_BG3CNT = 0;
+    REG_BG2CNT = 0;
+    REG_BG1CNT = 0;
+    REG_BG0CNT = 0;
+    REG_BG3HOFS = 0;
+    REG_BG3VOFS = 0;
+    REG_BG2HOFS = 0;
+    REG_BG2VOFS = 0;
+    REG_BG1HOFS = 0;
+    REG_BG1VOFS = 0;
+    REG_BG0HOFS = 0;
+    REG_BG0VOFS = 0;
+    DmaFill16Large(3, 0, VRAM, VRAM_SIZE, 0x1000);
+    DmaFill32Defvars(3, 0, OAM, OAM_SIZE);
+    DmaFill16Defvars(3, 0, PLTT, PLTT_SIZE);
+    ResetPaletteFade();
+    ScanlineEffect_Stop();
+    ResetTasks();
+    ResetSpriteData();
+    ResetPaletteFade(); // I really don't want palette fade here
+    FreeAllSpritePalettes();
+    LoadSpriteSheet(stru_83F8828);
+    LoadSpritePalette(stru_83F8838);
+    Text_LoadWindowTemplate(&gWindowTemplate_81E6C3C);
+    InitMenuWindow(&gWindowTemplate_81E6CE4);
+}
 
 #endif
