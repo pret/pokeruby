ca09158b73ba10b566db1f815ea2d826fc0f9024
PikalaxALT <pikalaxalt@gmail.com>
2018-04-25 15:11:44 -0400

    Decompile remaining easy chat data

---
+++ src/data/text/easy_chat/groups/trainer/order.h
@@ -0,0 +1,50 @@
+#ifndef DATA_TEXT_EASY_CHAT_GROUPS_TRAINER_ORDER
+#define DATA_TEXT_EASY_CHAT_GROUPS_TRAINER_ORDER
+
+#if ENGLISH
+u16 gEasyChatGroupOrder_Trainer[] = {
+    7,
+    8,
+    5,
+    4,
+    15,
+    1,
+    0,
+    18,
+    9,
+    6,
+    16,
+    14,
+    13,
+    17,
+    3,
+    10,
+    2,
+    11,
+    12,
+};
+#elif GERMAN
+u16 gEasyChatGroupOrder_Trainer[] = {
+    15,
+    7,
+    0,
+    8,
+    4,
+    5,
+    10,
+    1,
+    18,
+    9,
+    16,
+    14,
+    13,
+    17,
+    3,
+    2,
+    11,
+    12,
+    6,
+};
+#endif // GAME_LANGUAGE
+
+#endif // DATA_TEXT_EASY_CHAT_GROUPS_TRAINER_ORDER
