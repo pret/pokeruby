79149e717ff19d1a96f76b3854fb39a84b0bed16
aplumafreak500 <aplumafreak500@gmail.com>
2018-09-29 13:20:25 -0400

    Add support for gbafix

--- config.mk
+++ config.mk
@@ -7,12 +7,19 @@ GAME_LANGUAGE ?= ENGLISH
 DEBUG         ?= 0
 COMPARE  ?= 1
 
+# For gbafix
+MAKER_CODE  := 01
+
 # Version
 ifeq ($(GAME_VERSION), RUBY)
   BUILD_NAME := ruby
+  TITLE      := POKEMON RUBY
+  GAME_CODE  := AXV
 else
 ifeq ($(GAME_VERSION), SAPPHIRE)
   BUILD_NAME := sapphire
+  TITLE      := POKEMON SAPP
+  GAME_CODE  := AXP
 else
   $(error unknown version $(GAME_VERSION))
 endif
@@ -36,9 +43,11 @@ endif
 # Language
 ifeq ($(GAME_LANGUAGE), ENGLISH)
   BUILD_NAME := $(BUILD_NAME)
+  GAME_CODE  := $(GAME_CODE)E
 else
 ifeq ($(GAME_LANGUAGE), GERMAN)
   BUILD_NAME := $(BUILD_NAME)_de
+  GAME_CODE  := $(GAME_CODE)D
 else
   $(error unknown language $(GAME_LANGUAGE))
 endif
