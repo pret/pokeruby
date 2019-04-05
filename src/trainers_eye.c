904caf8d4040cc473415c3263ac8159266dcc7a2
PikalaxALT <pikalaxalt@gmail.com>
2019-04-05 09:38:26 -0400

    Decompile sGymLeaderTrainersEye

--- src/trainers_eye.c
+++ src/trainers_eye.c
@@ -2,6 +2,8 @@
 #include "battle_setup.h"
 #include "overworld.h"
 #include "pokenav.h"
+#include "constants/opponents.h"
+#include "constants/region_map_sections.h"
 
 struct TrainersEyeGymLeadersAndE4
 {
@@ -9,7 +11,21 @@ struct TrainersEyeGymLeadersAndE4
     u16 regionMapSectionId;
 };
 
-extern const struct TrainersEyeGymLeadersAndE4 gUnknown_083E49C0[13];
+static const struct TrainersEyeGymLeadersAndE4 sGymLeaderTrainersEye[13] = {
+    {TRAINER_ROXANNE,       MAPSEC_RUSTBORO_CITY},
+    {TRAINER_BRAWLY,        MAPSEC_DEWFORD_TOWN},
+    {TRAINER_WATTSON,       MAPSEC_MAUVILLE_CITY},
+    {TRAINER_FLANNERY,      MAPSEC_LAVARIDGE_TOWN},
+    {TRAINER_NORMAN,        MAPSEC_PETALBURG_CITY},
+    {TRAINER_WINONA,        MAPSEC_FORTREE_CITY},
+    {TRAINER_TATE_AND_LIZA, MAPSEC_MOSSDEEP_CITY},
+    {TRAINER_WALLACE,       MAPSEC_SOOTOPOLIS_CITY},
+    {TRAINER_SIDNEY,        MAPSEC_EVER_GRANDE_CITY},
+    {TRAINER_PHOEBE,        MAPSEC_EVER_GRANDE_CITY},
+    {TRAINER_GLACIA,        MAPSEC_EVER_GRANDE_CITY},
+    {TRAINER_DRAKE,         MAPSEC_EVER_GRANDE_CITY},
+    {TRAINER_STEVEN,        MAPSEC_EVER_GRANDE_CITY}
+};
 
 void sub_80F6C20(void)
 {
@@ -32,10 +48,10 @@ void sub_80F6C20(void)
     for (i = 0; i < 13; i++)
     {
         {
-            if (HasTrainerAlreadyBeenFought(gUnknown_083E49C0[i].opponentId))
+            if (HasTrainerAlreadyBeenFought(sGymLeaderTrainersEye[i].opponentId))
             {
-                gUnknown_083DFEC4->unkCEE8[gUnknown_083DFEC4->unkD158].unk0 = gUnknown_083E49C0[i].opponentId;
-                gUnknown_083DFEC4->unkCEE8[gUnknown_083DFEC4->unkD158].unk2 = gUnknown_083E49C0[i].regionMapSectionId;
+                gUnknown_083DFEC4->unkCEE8[gUnknown_083DFEC4->unkD158].unk0 = sGymLeaderTrainersEye[i].opponentId;
+                gUnknown_083DFEC4->unkCEE8[gUnknown_083DFEC4->unkD158].unk2 = sGymLeaderTrainersEye[i].regionMapSectionId;
                 gUnknown_083DFEC4->unkCEE8[gUnknown_083DFEC4->unkD158].unk5 = 0;
                 gUnknown_083DFEC4->unkCEE8[gUnknown_083DFEC4->unkD158].unk4 = i + 0x38;
                 gUnknown_083DFEC4->unkD158++;
