c11469b4ae2561e1d6d00744d5389735da014cb4
PikalaxALT <pikalaxalt@gmail.com>
2019-06-27 13:25:53 -0400

    through gUnknown_083F8EBC

--- include/roulette_util.h
+++ include/roulette_util.h
@@ -4,7 +4,7 @@
 #include "roulette.h"
 
 void sub_8124918(struct UnkStruct0 *);
-u8 sub_812492C(struct UnkStruct0 *, u8, struct UnkStruct1 *);
+u8 sub_812492C(struct UnkStruct0 *, u8, const struct UnkStruct1 *);
 void task_tutorial_controls_fadein(struct UnkStruct0 *);
 void sub_8124CE8(struct UnkStruct0 *, u16);
 void sub_8124D3C(struct UnkStruct0 *, u16);
