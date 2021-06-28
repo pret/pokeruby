f90a42fa5248c61fef9bf1c485f479314da176cd
PikalaxALT <pikalaxalt@gmail.com>
2021-06-28 09:05:31 -0400

    Resolve ewram ptr in mail.c

--- include/ewram.h
+++ include/ewram.h
@@ -14,7 +14,6 @@ extern u8 gSharedMem[];
 #define gBattleStruct                   ((struct BattleStruct *)     (gSharedMem + 0x0))
 #define namingScreenData                (*(struct NamingScreenData *)(gSharedMem + 0x0))
 #define ewramBerryPic                   (gSharedMem + 0x0)
-#define ewram0_4                        (*(struct Unk2000000 *)(gSharedMem + 0x0))
 #define ewram0_5                        (*(struct UnkStruct *)(gSharedMem + 0x0))
 #define eMatsudaDebugVar                (gSharedMem[0x0])
 #define eBrendanSprite                  (gSharedMem + 0x0)
