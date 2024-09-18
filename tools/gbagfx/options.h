7505574a7d108046bf7df5dd2a5e6c9d20cf8637
GriffinR <griffin.g.richards@gmail.com>
2024-09-18 00:21:11 -0400

    Sync tools

--- tools/gbagfx/options.h
+++ tools/gbagfx/options.h
@@ -4,6 +4,7 @@
 #define OPTIONS_H
 
 #include <stdbool.h>
+#include "gfx.h"
 
 struct GbaToPngOptions {
     char *paletteFilePath;
@@ -12,13 +13,22 @@ struct GbaToPngOptions {
     int width;
     int metatileWidth;
     int metatileHeight;
+    char *tilemapFilePath;
+    bool isAffineMap;
+    bool isTiled;
+    int dataWidth;
 };
 
 struct PngToGbaOptions {
     int numTiles;
+    enum NumTilesMode numTilesMode;
     int bitDepth;
     int metatileWidth;
     int metatileHeight;
+    char *tilemapFilePath;
+    bool isAffineMap;
+    bool isTiled;
+    int dataWidth;
 };
 
 #endif // OPTIONS_H
