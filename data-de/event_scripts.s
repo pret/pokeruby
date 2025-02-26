7638fb93ce55a7518ef4246b11b0b9d557e69d65
Robin Xyifer <xyifer@gmail.com>
2025-02-26 16:28:07 -0600

    Reverted change, AbnormalWeather is now LegendaryWeather.

--- data-de/event_scripts.s
+++ data-de/event_scripts.s
@@ -859,7 +859,7 @@ Common_EventScript_NoRoomForDecor:: @ 81A02B8
 	msgbox Text_NoRoomLeftForAnother, MSGBOX_DEFAULT
 	return
 
-Common_EventScript_SetAbnormalWeather:: @ 81A02C1
+Common_EventScript_SetLegendaryWeather:: @ 81A02C1
 	.ifdef SAPPHIRE
 	setweather WEATHER_RAIN_HEAVY
 	.else
