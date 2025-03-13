18784d3a27d309977ed0c0953800529d6fb43736
Robin Xyifer <xyifer@gmail.com>
2025-03-13 18:59:56 -0500

    Revision and cleanup before build testing.

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -122,7 +122,7 @@
 #define FLAG_RECEIVED_TM34                  0xA7
 #define FLAG_RECEIVED_TM_OVERHEAT                  0xA8
 #define FLAG_RECEIVED_TM42                  0xA9
-#define FLAG_RECEIVED_TM40                  0xAA
+#define FLAG_RECEIVED_TM_AERIAL_ACE                  0xAA
 #define FLAG_RECEIVED_TM04                  0xAB
 #define FLAG_RECEIVED_TM03                  0xAC
 #define FLAG_DECORATION_1                   0xAD
